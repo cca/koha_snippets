4dcfc61b88c14fde0e2a2491f90aff23593e25c5
phette23 <phette23@gmail.com>
2016-05-31 15:48:09 -0700

    initial commit - ISBN search redirect

---
+++ js/isbn-redirect.js
@@ -0,0 +1,17 @@
+// redirect old Millennium ISBN queries (e.g. from website, external places)
+// to Koha's new search URL structure
+
+var path = location.pathname
+// Millennium ISBN/ISSN search URLs look like
+// http://library.cca.edu/search~S2/i?SEARCH=9781118903339
+// so we check the path for similar
+if (path.match(/\/search~S\d\/i/)) {
+    var standard_no = location.search.split('=')[1]
+    // put up a notice otherwise 404 page displays, a bit confusing
+    document.body.innerHTML = '<h1>Redirecting your request...</h1>'
+    // Redirect to Koha's equivalent URL
+    // Koha ISBN search, idx=nb -> ISBN index, q is query param
+    // NOTE: ISSN index is different (idx=ns) & the combined "standard number"
+    // index idx=sn doesn't seem to work, so only ISBN redirects will work
+    location.pathname = '/cgi-bin/koha/opac-search.pl?idx=nb&q=' + standard_no
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Koha Snippets
+
+Snippets of code to be inserted into the administrative settings of Koha.
+
+## Development
+
+I'm using npm scripts to automate development tasks. Run `npm install` to get the necessary dependencies. Then, from inside this project, you'll have these `npm run` scripts available:
+
+- `build` - run all the compilation scripts listed here, meta-task
+- `js` - minify all JavaScript in the JS folder & wrap it in a closure
+
+All output goes in the "dist" directory unless otherwise noted.
+
+The tasks above let me write many, small snippets of code which accomplish one thing and then combine them all together into a single optimized file.
+
+## License
+
+[ECL-2.0](https://opensource.org/licenses/ECL-2.0)
