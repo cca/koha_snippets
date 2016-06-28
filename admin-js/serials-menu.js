39572b8ad94059a8fd7201b3d33d00b75e845f73
phette23 <phette23@gmail.com>
2016-06-28 09:47:48 -0700

    serials menu links

--- admin-js/serials-menu.js
+++ admin-js/serials-menu.js
@@ -4,16 +4,24 @@ if (location.pathname.match('/cgi-bin/koha/serials/')) {
     // no convenient style hooks here
     var menu_list = $('#bd > .yui-b ul')
     var links = [
-        ['text', 'url']
-        , ['text 2', 'url 2']
-        , ['text 3', 'url 3']
+        ['EBSCONET',
+        'https://ebsconet.com']
+        , ['Serials Solutions',
+        'https://clientcenter.serialssolutions.com/CC/Login/Default.aspx']
+        , ['SF Late for Claiming',
+        '/cgi-bin/koha/reports/guided_reports.pl?reports=64&phase=Show%20SQL']
+        , ['OAK Late for Claiming',
+        '/cgi-bin/koha/reports/guided_reports.pl?reports=63&phase=Show%20SQL']
+        , ['SF at bindery',
+        '/cgi-bin/koha/reports/guided_reports.pl?reports=68&phase=Show%20SQL']
+        , ['OAK at bindery',
+        '/cgi-bin/koha/reports/guided_reports.pl?reports=69']
     ]
     var len = links.length
-    var html = ''
     for (var i = 0; i < len; i++) {
-        html += '<li><a href="' + links[i][1]
+        var html = '<li><a href="' + links[i][1]
             + '" target="_blank">' + links[i][0]
             + '</a></li>'
+        menu_list.prepend(html)
     }
-    menu_list.html(html)
 }
