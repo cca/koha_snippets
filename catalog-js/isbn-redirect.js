fd897b73bd4a31277e8cf7905b956dade36b2d8b
phette23 <phette23@gmail.com>
2016-06-29 15:12:59 -0700

    redirect Millennium keyword searches

--- catalog-js/isbn-redirect.js
+++ catalog-js/isbn-redirect.js
@@ -2,18 +2,29 @@
 // to Koha's new search URL structure
 (function() {
 
+function redir(path) {
+    // put up a notice otherwise 404 page displays, a bit confusing
+    document.body.innerHTML = '<h1>Redirecting your request...</h1>'
+    // go to new location
+    location = path
+}
+
 // Millennium ISBN/ISSN search URLs look like
 // http://library.cca.edu/search~S2/i?SEARCH=9781118903339
 // so we check the path for similar
 if (location.pathname.match(/\/search~S\d\/i/)) {
     var standard_no = location.search.split('=')[1]
-    // put up a notice otherwise 404 page displays, a bit confusing
-    document.body.innerHTML = '<h1>Redirecting your request...</h1>'
     // Redirect to Koha's equivalent URL
     // Koha ISBN search, idx=nb -> ISBN index, q is query param
     // NOTE: ISSN index is different (idx=ns) & the combined "standard number"
     // index idx=sn doesn't seem to work, so only ISBN redirects will work
-    location.pathname = '/cgi-bin/koha/opac-search.pl?idx=nb&q=' + standard_no
+    redir('/cgi-bin/koha/opac-search.pl?idx=nb&q=' + standard_no)
+
+    // Millennium keyword type searches
+} else if (location.toString().match(/\/search~S\d\?\/Y/i)) {
+    var kw = location.search.split('?/Y')[1].split('&')[0]
+    // redirect to Koha keyword search, idx=kw
+    redir('/cgi-bin/koha/opac-search.pl?idx=kw,wrdl&q=' + kw)
 }
 
 })()
