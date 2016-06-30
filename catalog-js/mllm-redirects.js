3d97e886272d16bc10aa836201bf3fe399749de7
phette23 <phette23@gmail.com>
2016-06-30 12:15:19 -0700

    big update to mllm redirects

--- catalog-js/mllm-redirects.js
+++ catalog-js/mllm-redirects.js
@@ -1,8 +1,27 @@
 // redirect old Millennium ISBN queries (e.g. from website, external places)
 // to Koha's new search URL structure
+// for details on Millennium search URL structure, see
+// http://csdirect.iii.com/manual_2009b/rmil_command_links.html
 (function() {
 
-function redir(path) {
+function getQuery () {
+    var url = location.toString()
+
+    // if we have etiher a "searcharg" or "SEARCH" query param, take it
+    if (url.match('searcharg')) {
+        return url.split('searcharg=')[1].split('&')[0]
+    } else if (url.match('SEARCH')) {
+        return url.split('SEARCH=')[1].split('&')[0]
+    } else {
+        // for URLs lacking a param, we look for the /d /a etc. index indicators
+        // & take whatever comes after them
+        // e.g. /search/aPhetteplace -> Phetteplace
+        // but work around the optional "~S2" scope parameter
+        return url.split(/\/search(~S\d\??)?\/\w/)[2].split(/\/\w/)[0]
+    }
+}
+
+function redir (path) {
     // put up a notice otherwise 404 page displays, a bit confusing
     document.body.innerHTML = '<h1>Redirecting your request...</h1>'
     // go to new location
@@ -10,9 +29,9 @@ function redir(path) {
 }
 
 // Millennium ISBN/ISSN search URLs look like
-// http://library.cca.edu/search~S2/i?SEARCH=9781118903339
+// /search~S2/i?SEARCH=9781118903339
 // so we check the path for similar
-if (location.pathname.match(/\/search~S\d\/i/)) {
+if (location.pathname.match(/\/search(~S\d\??)?\/i/)) {
     var standard_no = location.search.split('=')[1]
     // Redirect to Koha's equivalent URL
     // Koha ISBN search, idx=nb -> ISBN index, q is query param
@@ -21,10 +40,28 @@ if (location.pathname.match(/\/search~S\d\/i/)) {
     redir('/cgi-bin/koha/opac-search.pl?idx=nb&q=' + standard_no)
 
     // Millennium keyword type searches
-} else if (location.toString().match(/\/search~S\d\?\/Y/i)) {
-    var kw = location.search.split('?/Y')[1].split('&')[0]
-    // redirect to Koha keyword search, idx=kw
-    redir('/cgi-bin/koha/opac-search.pl?idx=kw,wrdl&q=' + kw)
+} else if (location.toString().match(/\/search(~S\d\??)?\/(Y|X|w)/)) {
+    var kw = getQuery()
+    kw && redir('/cgi-bin/koha/opac-search.pl?q=' + kw)
+
+    // Millennium subject searches
+    // URL like /search~S1?/dteaching/dteaching/1%2C8%2C11%2CB/exact&FF=dteaching&1%2C4%2C
+} else if (location.toString().match(/\/search(~S\d\??)?\/d/)) {
+    var subj = getQuery()
+    // redirect to Koha subject search, looks like q=(su:{Subject Term})
+    subj && redir('/cgi-bin/koha/opac-search.pl?q=(su:%7B' + subj + '%7D)')
+
+    // Millennium author searches
+} else if (location.toString().match(/\/search(~S\d\??)?\/a/)) {
+    var author = getQuery()
+    // redirect to Koha author search, looks like q=(au:{Author Name})
+    author && redir('/cgi-bin/koha/opac-search.pl?q=(au:%7B' + author + '%7D)')
+
+    // Millennium title searches
+} else if (location.toString().match(/\/search(~S\d\??)?\/t/)) {
+    var title = getQuery()
+    // redirect to Koha title search, looks like q=(ti:{Title of Work})
+    title && redir('/cgi-bin/koha/opac-search.pl?q=(ti:%7B' + title + '%7D)')
 }
 
 })()
