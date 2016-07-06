b528d7cb216a113b8b8998b394352cb570e852e9
phette23 <phette23@gmail.com>
2016-07-06 15:50:05 -0700

    correct require([],fn) usage

--- admin-js/942-c-default-value.js
+++ admin-js/942-c-default-value.js
@@ -7,16 +7,17 @@ if (location.pathname.match('/cgi-bin/koha/cataloguing/editor.pl')) {
         // this module is in koha-tmpl/intranet-tmpl/lib/koha/cateditor/koha-backend.js
         // & you can see how it's used in the cataloging module at
         // koha-tmpl/intranet-tmpl/prog/en/includes/cateditor-ui.inc
-        var kohaBackend = require('koha-backend')
-        try {
-            // GetTagInfo signature is (framework code, tag)
-            // where empty string => default framework
-            var value = kohaBackend.GetTagInfo('', 942).subfields.c.defaultvalue
-        } catch (e) {
-            console.error('error trying to get 942$c default value', e)
-        }
-        if (value) {
-            $('.subfield-widget option[value="' + value + '"]').prop('selected', true)
-        }
+        require(['koha-backend'], function (kohaBackend) {
+            try {
+                // GetTagInfo signature is (framework code, tag)
+                // where empty string => default framework
+                var value = kohaBackend.GetTagInfo('', 942).subfields.c.defaultvalue
+            } catch (e) {
+                console.error('error trying to get 942$c default value', e)
+            }
+            if (value) {
+                $('.subfield-widget option[value="' + value + '"]').prop('selected', true)
+            }
+        })
     })
 }
