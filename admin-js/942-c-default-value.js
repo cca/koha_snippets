a5414a3b41f432a5baacdb76e218eab89cbbcbee
phette23 <phette23@gmail.com>
2016-07-06 14:01:09 -0700

    set correct 942$c default in drop down
    use koha-backend module to pull MARC framework information
    then set the selected <option>

---
+++ admin-js/942-c-default-value.js
@@ -0,0 +1,22 @@
+// set correct default for 942$c field
+// we have jQuery available, run only on advanced cataloging editor
+if (location.pathname.match('/cgi-bin/koha/cataloguing/editor.pl')) {
+    // run on document load
+    $(function(){
+        // load KohaBackend module which pulls JSON MARC framework data
+        // this module is in koha-tmpl/intranet-tmpl/lib/koha/cateditor/koha-backend.js
+        // & you can see how it's used in the cataloging module at
+        // koha-tmpl/intranet-tmpl/prog/en/includes/cateditor-ui.inc
+        var kohaBackend = require('koha-backend')
+        try {
+            // GetTagInfo signature is (framework code, tag)
+            // where empty string => default framework
+            var value = kohaBackend.GetTagInfo('', 942).subfields.c.defaultvalue
+        } catch (e) {
+            console.error('error trying to get 942$c default value', e)
+        }
+        if (value) {
+            $('.subfield-widget option[value="' + value + '"]').prop('selected', true)
+        }
+    })
+}
