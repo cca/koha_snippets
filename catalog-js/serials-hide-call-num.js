61311d5261adacd6fa136f5093b6dbad8b671ecd
phette23 <phette23@gmail.com>
2016-07-11 16:06:58 -0700

    revert hiding serials call numbers

--- catalog-js/serials-hide-call-num.js
+++
@@ -1,7 +0,0 @@
-// hide unused "Call Number" column on a type=serials detail page
-if (location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
-    // only for mat type "cont. resource"
-    if ($('.results_summary.type').text().match('Continuing resource')) {
-        $('.call_no').css('display' , 'none')
-    }
-}
