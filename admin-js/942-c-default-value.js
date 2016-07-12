a68059d7364e03ad1a010a925d6cc258602049c1
phette23 <phette23@gmail.com>
2016-07-11 17:05:17 -0700

    new attempt at fixing 942$c default value

--- admin-js/942-c-default-value.js
+++ admin-js/942-c-default-value.js
@@ -1,29 +1,35 @@
 // set correct default for 942$c field
 // we have jQuery available, run only on advanced cataloging editor
 if (location.pathname.match('/cgi-bin/koha/cataloguing/editor.pl')) {
-    // run on document load
-    $(function(){
-        // load KohaBackend module which pulls JSON MARC framework data
-        // this module is in koha-tmpl/intranet-tmpl/lib/koha/cateditor/koha-backend.js
-        // & you can see how it's used in the cataloging module at
-        // koha-tmpl/intranet-tmpl/prog/en/includes/cateditor-ui.inc
-        require(['koha-backend'], function (kohaBackend) {
-            try {
-                // GetTagInfo signature is (framework code, tag)
-                // where empty string => default framework
-                var value = kohaBackend.GetTagInfo('', 942).subfields.c.defaultvalue
-                // check if the option list has loaded every 500ms
-                var interval = setInterval(function(){
-                    var subf = $('.subfield-widget option[value="' + value + '"]')
-                    if (subf.length > 0) {
-                        subf.prop('selected', true)
-                        // stop checking
-                        clearInterval(interval)
-                    }
-                }, 500)
-            } catch (e) {
-                console.error('error trying to get 942$c default value', e)
+    // set 942c _once it appears in the DOM_ (check at intervals)
+    var set942c = function (itype) {
+        var interval = setInterval(function(){
+            var subf = $('.subfield-widget option[value="' + itype + '"]')
+            if (subf.length > 0) {
+                subf.prop('selected', true)
+                // stop checking
+                clearInterval(interval)
             }
+        }, 500)
+    }
+
+    var biblionumber = location.hash.split('/')[1]
+    // use Koha web service to get MARC record
+    $.get('/cgi-bin/koha/svc/bib/' + biblionumber)
+        .done(function(xml){
+            // parse MARCXML to find 942$c
+            var itype = $('datafield[tag="942"] subfield[code="c"]', xml).text()
+            // run only after document load
+            $(function(){
+                set942c(itype)
+                // also, whenever record is saved, reset 942c to correct value
+                // also works for Ctrl+S shortcut b/c that just clicks #save-record
+                $('#save-record').click(function(){
+                    set942c(itype)
+                })
+            })
         })
-    })
+        .fail(function(error){
+            console.error('error getting MARC preview', error)
+        })
 }
