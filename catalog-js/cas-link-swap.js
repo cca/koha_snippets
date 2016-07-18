08a2016c591cb24e218b0949dc0be502bd629065
phette23 <phette23@gmail.com>
2016-07-18 12:15:33 -0700

    CAS link is now sso.cca.edu

--- catalog-js/cas-link-swap.js
+++ catalog-js/cas-link-swap.js
@@ -1,3 +1,3 @@
-$('#opac-auth a[href*="cas.cca.edu"]').attr('href', function(pos, val) {
+$('#opac-auth a[href*="sso.cca.edu"]').attr('href', function(pos, val) {
     return val.replace('library.cca.edu%2Fopac', 'library.cca.edu%2Fcgi-bin%2Fkoha')
 });
