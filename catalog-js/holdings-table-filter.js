ae2f69b1eb0718a9d97e0cb46c07efb1f6de38c9
phette23 <phette23@gmail.com>
2016-08-02 16:47:43 -0700

    filter for large holdings tables

---
+++ catalog-js/holdings-table-filter.js
@@ -0,0 +1,35 @@
+// add a filter to large holdings tables so users can find the issue/volume
+// they want more easily, very useful for serials with large numbers of items
+
+// we're on a details page & there are more than 4 rows of holdings (not counting header row)
+if (location.pathname === '/cgi-bin/koha/opac-detail.pl' && $('.holdingst tr').length > 5) {
+    // run on document load
+    $(function(){
+        // "aoColumns" passage copied from details page source
+        $(".holdingst").dataTable($.extend(true, {}, dataTablesDefaults, {
+            "aoColumns": [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                { "sType": "title-string" },
+                null
+            ],
+            // dataTables throws an error if you overwrite an existing table
+            // "destroy" tells it to delete & recreate table
+            "destroy": true,
+            // the magic sauce: "f" tells DT to write the filter onto the DOM
+            "sDom": "ft"
+        }));
+        // customize filter appearance a bit
+        $('.dataTables_filter').find('label').css('font-weight', 'bold')
+            .find('input').css({
+                'margin-left': '1em',
+                'width': '20em'
+            })
+            .attr('placeholder', 'for a particular volume or issue…')
+    })
+}
