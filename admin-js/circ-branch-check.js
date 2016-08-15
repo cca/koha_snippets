fa0d2d470a34c255a3a2a1ebbb06a8ec9f28aa49
phette23 <phette23@gmail.com>
2016-08-15 12:26:01 -0700

    warning when item is checked in away from home
    fixes #2

---
+++ admin-js/circ-branch-check.js
@@ -0,0 +1,19 @@
+// run only on the checkin page
+if (location.pathname.match('/cgi-bin/koha/circ/returns.pl')) {
+    // run on document load
+    $(function(){
+        // loop over each row in the checkin table
+        $('#checkedintable tr').each(function(){
+            var row = $(this)
+            var home = row.find('td.ci-homelibrary')
+            var holding = row.find('td.ci-holdinglibrary')
+            // compare home branch to branch where the item's being checked in
+            if (home.text() !== holding.text()) {
+                // if they differ, show an alert with on-hover helper text
+                holding.addClass('error alert-error')
+                    .css({'cursor': 'help', 'font-weight': 'bold'})
+                    .attr('title', 'this item needs to be returned to its home library')
+            }
+        })
+    })
+}
