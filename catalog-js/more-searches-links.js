fe86a63e22d4675669d8cba70de157432d82ae61
phette23 <phette23@gmail.com>
2016-08-19 10:51:11 -0700

    hours in opacnavbottom, links in #mainsearches

--- catalog-js/more-searches-links.js
+++ catalog-js/more-searches-links.js
@@ -1,2 +1,5 @@
 // this is the gray bar beneath the search box that appears on every page
-$('#moresearches ul').append('<li><a href="https://libraries.cca.edu">Library Home</a></li>')
+$('#moresearches ul')
+    .append('<li><a href="https://libraries.cca.edu/contact">Ask a Librarian</a></li>')
+    .append('<li><a href="http://libguides.cca.edu">Research Guides</a></li>')
+    .append('<li><a href="https://libraries.cca.edu">Library Home</a></li>')
