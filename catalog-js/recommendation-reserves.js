1cfd508cb2daca0dcdf910c394b357d6876ae323
phette23 <phette23@gmail.com>
2016-09-09 10:52:27 -0700

    course reserves link on purchase rec form

---
+++ catalog-js/recommendation-reserves.js
@@ -0,0 +1,6 @@
+// add link to reserves page on purchase recommendation form
+// need to escape question mark in href here
+if (location.href.match('cgi-bin/koha/opac-suggestions.pl\\?op=add')) {
+    var html = '<p>If this purchase recommedation is meant for course reserves, remember to <a href="https://libraries.cca.edu/content/course-reserves-request">fill out the Course Reserves Request</a> form, too.'
+    $('#usersuggestions form').before(html)
+}
