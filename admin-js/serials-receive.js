3b8ad4f8a010143078c6d718d70f0a9a63e64ffd
phette23 <phette23@gmail.com>
2017-01-18 17:25:24 -0800

    make serials receive edits more careful

--- admin-js/serials-receive.js
+++ admin-js/serials-receive.js
@@ -2,22 +2,26 @@
 if (location.pathname.match('/cgi-bin/koha/serials/serials-edit.pl')) {
     // run on document load
     $(function(){
-        // g - Cost, normal purchased price &
-        // v - Cost, replacement price both default to "15.00"
-        $('input[id^="tag_952_subfield_g"]').val('15.00')
-        // yes, this is insane, I couldn't find a better way to isolate the subfield
-        $('div[id$="v"]').filter('div[id^="subfield"]').find('input').val('15.00')
-        // hide: 8 - collection code, j - shelving control no., t - copy number
-        $('input[name="subfield"]')
-            .filter('[value="8"], [value="j"], [value="t"]')
-            .parentsUntil('li')
-            .hide()
-        // y - Koha item type defaults to "NEWPER" (Current Periodical)
-        $('option[value="NEWPER"]').prop('selected', true)
-        // highlight the barcode input
-        $('label:contains("p - Barcode")').css('font-size', '120%')
-            .next('input').css('box-shadow', '0px 0px 3px 3px #41ff3e')
-            // remove the useless "..." link
-            .next('a').hide()
+        // when a new issue is about to be received, fill in values
+        $('a:contains("Click to add item")').on('click', function(){
+            var item = $(this).next('fieldset')
+            // g - Cost, normal purchased price &
+            // v - Cost, replacement price both default to "15.00"
+            item.find('input[id^="tag_952_subfield_g"]').val('15.00')
+            // yes, this is insane, I couldn't find a better way to isolate the subfield
+            item.find('div[id$="v"]').filter('div[id^="subfield"]').find('input').val('15.00')
+            // hide: 8 - collection code, j - shelving control no., t - copy number
+            item.find('input[name="subfield"]')
+                .filter('[value="8"], [value="j"], [value="t"]')
+                .parentsUntil('li')
+                .hide()
+            // y - Koha item type defaults to "NEWPER" (Current Periodical)
+            item.find('option[value="NEWPER"]').prop('selected', true)
+            // highlight the barcode input
+            item.find('label:contains("p - Barcode")').css('font-size', '120%')
+                .next('input').css('box-shadow', '0px 0px 3px 3px #41ff3e')
+                // remove the useless "..." link
+                .next('a').hide()
+        })  
     })
 }
