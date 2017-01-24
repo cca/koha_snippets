d683a28b9db61991874a27490c77b28a77196c45
phette23 <phette23@gmail.com>
2017-01-23 16:57:29 -0800

    suggestions - fix datatable trigger issue
    previously customizations weren't applied because datatables
    weren't in the DOM yet, wait until they're there before applying

--- admin-js/suggestions.js
+++ admin-js/suggestions.js
@@ -1,10 +1,24 @@
 // customizations for staff-side suggestions management page
 if (location.pathname.match('/cgi-bin/koha/suggestion/suggestion.pl')) {
-	// run on document load
 	$(function(){
-		// default all "show ___ entries" <select> inputs to "All"
-		$('select[name$="t_length"] option[value="-1"]').prop('selected', true)
-		// default the Acquisition Information library branch to "any"
-		$('#branchcode option[value="__ANY__"]').prop('selected', true)
+		// silly approach: we need to customize after all datatables are in the
+		// DOM but only one init.dt event fires (?), an order.dt event fires for
+		// each table so we count the number of tables & run after last order.t
+		// event, then remove the event handler itself
+		var numTabs = $('#suggestiontabs li').length
+		// these are namespaced custom datatables events
+		$(document).on('order.dt', function(){
+			console.log('order.dt', numTabs)
+			if (numTabs === 1) {
+				// default all "show ___ entries" <select> inputs to "All"
+				$('select[name$="t_length"] option[value="-1"]').prop('selected', true)
+				// default the Acquisition Information library branch to "any"
+				$('#branchcode option[value="__ANY__"]').prop('selected', true)
+				// event handler erase thyself
+				$(document).off('order.dt')
+			} else {
+				numTabs--
+			}
+		})
 	})
 }
