a3cc55843f134631a33b8c020c58a9fd985f573b
phette23 <phette23@gmail.com>
2017-02-03 14:53:11 -0800

    perf - only load coverflow script when on home pg

--- catalog-js/coverflow.js
+++ catalog-js/coverflow.js
@@ -1,41 +1,44 @@
 /* @preserve JS for Koha CoverFlow Plugin
    This JS was added automatically by installing the CoverFlow plugin
    Please do not modify */
-$(document).ready(function() {
-    $.getScript(
-        "/plugin/Koha/Plugin/Com/ByWaterSolutions/CoverFlow/bower_components/jquery-flipster/dist/jquery.flipster.min.js",
-        function(data, textStatus, jqxhr) {
-            $("head").append(
-                "<link id='flipster-css' href='/plugin/Koha/Plugin/Com/ByWaterSolutions/CoverFlow/bower_components/jquery-flipster/dist/jquery.flipster.min.css' type='text/css' rel='stylesheet' />"
-            );
-            $('#coverflow').load("/coverflow.pl?id=127", function() {
-                $('.koha-coverflow img').on("load",
-                    function() {
-                        if (this.naturalHeight == 1) {
-                            $(this).attr("src",
-                                "http://media.bywatersolutions.com/Model/NoImage.png"
-                            );
+// only run on the home page, triggers unnecessary HTTP request otherwise
+if (location.pathname.match('/cgi-bin/koha/opac-main.pl') || location.pathname === '/') {
+    $(function() {
+        $.getScript(
+            "/plugin/Koha/Plugin/Com/ByWaterSolutions/CoverFlow/bower_components/jquery-flipster/dist/jquery.flipster.min.js",
+            function(data, textStatus, jqxhr) {
+                $("head").append(
+                    "<link id='flipster-css' href='/plugin/Koha/Plugin/Com/ByWaterSolutions/CoverFlow/bower_components/jquery-flipster/dist/jquery.flipster.min.css' type='text/css' rel='stylesheet' />"
+                );
+                $('#coverflow').load("/coverflow.pl?id=127", function() {
+                    $('.koha-coverflow img').on("load",
+                        function() {
+                            if (this.naturalHeight == 1) {
+                                $(this).attr("src",
+                                    "http://media.bywatersolutions.com/Model/NoImage.png"
+                                );
+                            }
+                        });
+                    var opt = {
+                        'items': '.item',
+                        'minfactor': 15,
+                        'distribution': 1.5,
+                        'scalethreshold': 0,
+                        'staticbelowthreshold': false,
+                        'titleclass': 'itemTitle',
+                        'selectedclass': 'selectedItem',
+                        'scrollactive': true,
+                        'step': {
+                            'limit': 4,
+                            'width': 10,
+                            'scale': true
                         }
+                    };
+                    $('#coverflow').flipster({
+                        autoplay: '4000',
+                        loop: 'true',
                     });
-                var opt = {
-                    'items': '.item',
-                    'minfactor': 15,
-                    'distribution': 1.5,
-                    'scalethreshold': 0,
-                    'staticbelowthreshold': false,
-                    'titleclass': 'itemTitle',
-                    'selectedclass': 'selectedItem',
-                    'scrollactive': true,
-                    'step': {
-                        'limit': 4,
-                        'width': 10,
-                        'scale': true
-                    }
-                };
-                $('#coverflow').flipster({
-                    autoplay: '4000',
-                    loop: 'true',
                 });
             });
-        });
-});
+    });
+}
