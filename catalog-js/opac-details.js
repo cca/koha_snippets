499970f117c5c8996f14add07d7ebfab544d18f7
phette23 <phette23@gmail.com>
2017-02-03 16:00:14 -0800

    catalog-js/

--- catalog-js/opac-details.js
+++ catalog-js/opac-details.js
@@ -3,6 +3,50 @@ if (location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
     // run on page load in own scope
     $(function(){
 
+        // if there are two cover images present, hide the second one
+        // lets us "fix" blurry/bad cover images by uploading our own
+        // we have to overwrite KOHA.LocalCover to accomplis this since
+        // images load dynamically & CSS has no "nth-descendent" type selector
+        if (typeof KOHA == "undefined" || !KOHA) window.KOHA = {}
+        // this function copied from /opac-tmpl/bootstrap/js/localcovers.js
+        KOHA.LocalCover.GetCoverFromBibnumber = function(uselink) {
+            $("div[id^=local-thumbnail],span[id^=local-thumbnail]").each(function(i) {
+                var mydiv = this;
+                var message = document.createElement("span");
+                $(message).attr("class","no-image");
+                $(message).html(NO_LOCAL_JACKET);
+                $(mydiv).parent().find('.no-image').remove();
+                $(mydiv).append(message);
+                var img = $("<img />").attr('src',
+                    '/cgi-bin/koha/opac-image.pl?thumbnail=1&biblionumber=' + $(mydiv).attr("class"))
+                    .load(function () {
+                        this.setAttribute("class", "thumbnail");
+                        if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+                            //IE HACK
+                            try {
+                                $(mydiv).append(img);
+                                $(mydiv).children('.no-image').remove();
+                            }
+                            catch (err) {
+                            };
+                        } else if (this.width > 1) { // don't show the silly 1px "no image" img
+                            if (uselink) {
+                                var a = $("<a />").attr('href', '/cgi-bin/koha/opac-imageviewer.pl?biblionumber=' + $(mydiv).attr("class"));
+                                $(a).append(img);
+                                $(mydiv).empty().append(a);
+                            } else {
+                                $(mydiv).empty().append(img);
+                            }
+                            $(mydiv).children('.no-image').remove();
+                            // remove the COCE cover image div
+                            $('#coce-thumbnail-preview').remove()
+                        }
+                    })
+            });
+        }
+        // execute function we just redefined
+        KOHA.LocalCover.GetCoverFromBibnumber(true)
+
         // fix 830$a series links ending in semicolons, see bug #14716
         $('.results_summary.series a').each(function(){
             // URI-encoded semicolon
