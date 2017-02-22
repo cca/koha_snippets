2b03af2564e0e551ccc717c0ea3dd784a01869ac
phette23 <phette23@gmail.com>
2017-02-22 12:54:01 -0800

    note what koha bug cas-link-swap relates to

--- catalog-js/cas-link-swap.js
+++ catalog-js/cas-link-swap.js
@@ -1,3 +1,5 @@
+// see Koha bug #16818 - Plack broke CAS login link
+// https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=16818
 $('#opac-auth a[href*="sso.cca.edu"]').attr('href', function(pos, val) {
     return val.replace('library.cca.edu%2Fopac', 'library.cca.edu%2Fcgi-bin%2Fkoha')
 });
