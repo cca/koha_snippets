96ea9ef00e5e6477b9192d805d71ca030b1e039c
phette23 <phette23@gmail.com>
2017-04-07 15:41:57 -0700

    let item.nonpublic_note be edited if it's non-empty

--- admin-js/item-nonpublic-note.js
+++ admin-js/item-nonpublic-note.js
@@ -4,10 +4,13 @@ if (location.href.match('cgi-bin/koha/catalogue/moredetail.pl')) {
     $(function(){
         // select nonpublic notes
         var npns = $('textarea[name="itemnotes_nonpublic"]')
-        npns.prop('disabled', true)
-        npns.css('box-shadow', '1px 1px 1px 1px yellow')
-        // emoji use ftw
-        npns.attr('title', 'Note: editing non-public notes here doesn\'t work. Use the "Edit Items" link under the "✏️ Edit" button to change it.')
-        npns.next('input.submit').prop('disabled', true)
+        // only disable the input if there's no data in it
+        if (npns.val() === '') {
+            npns.prop('disabled', true)
+            npns.css('box-shadow', '1px 1px 1px 1px yellow')
+            // emoji use ftw
+            npns.attr('title', 'Note: editing non-public notes here doesn\'t work. Use the "Edit Items" link under the "✏️ Edit" button to change it.')
+            npns.next('input.submit').prop('disabled', true)
+        }
     })
 }
