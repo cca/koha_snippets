b6ba37125e35605276e3d5a57c47d209284562aa
phette23 <phette23@gmail.com>
2017-09-25 16:19:09 -0700

    fix public list login link
    previous code was firing too early & not removing the onclick handler

--- catalog-js/list-login-link.js
+++ catalog-js/list-login-link.js
@@ -1,7 +1,11 @@
 // the login link under "Lists" pops up a modal that's only useful for people
 // with internal accounts, it has no CAS login. Replace it with a CAS login
 // link instead. Only want to run this if we're not logged in already.
-if ($('.loggedinusername').length > 0) {
-    $('.dropdown a.loginModal-trigger').off('click')
-        .attr('href', 'https://sso.cca.edu/cas/login?service=https%3A%2F%2Flibrary.cca.edu%2Fcgi-bin%2Fkoha%2Fopac-user.pl')
+if ($('.loggedinusername').length === 0) {
+    // a modest delay seems to ensure the onclick handler is there to be removed
+    // tried using document.load & a 400ms delay, neither worked
+    setTimeout(function(){
+        $('.loginModal-trigger').off('click')
+            .attr('href', 'https://sso.cca.edu/cas/login?service=https%3A%2F%2Flibrary.cca.edu%2Fcgi-bin%2Fkoha%2Fopac-user.pl')
+    }, 800)
 }
