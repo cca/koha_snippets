6f22d1d7c553973aa0c48300da12cf38d773fcc0
phette23 <phette23@gmail.com>
2017-11-15 16:03:09 -0800

    some updated icon URLs

--- catalog-js/item-type-icons.js
+++ catalog-js/item-type-icons.js
@@ -1,18 +1,18 @@
 // use our custom-made icons instead of default theme Koha ones
 if (location.pathname.match('/cgi-bin/koha/opac-search.pl')) {
     let icon_map = {
-        'book': 'book',
-        'other materials': 'other',
-        'reserve': 'reserve',
-        'serial': 'periodical',
-        'periodicals': 'periodical',
-        'video': 'video',
-        'visual material': 'video'
+        'book': 'book.svg',
+        'other materials': 'other-40',
+        'reserve': 'reserved-book.svg',
+        'serial': 'periodical.svg',
+        'periodicals': 'periodical.svg',
+        'video': 'video-40',
+        'visual material': 'video-40'
     }
     let replaceIcon = function (img) {
         for (let type of Object.keys(icon_map)) {
             if (img.alt.toLowerCase().match(type)) {
-                img.src = `https://libraries.cca.edu/media/images/${icon_map[type]}-40.original.png`
+                img.src = `https://libraries.cca.edu/media/images/${icon_map[type]}.original.png`
                 break
             }
         }
