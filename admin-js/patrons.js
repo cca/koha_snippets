0f976277174985cbff33ae81ef6f1db739dfcf84
phette23 <phette23@gmail.com>
2018-03-13 16:42:11 -0700

    patrons - don't actually need to add univ id to quick add form

--- admin-js/patrons.js
+++ admin-js/patrons.js
@@ -2,22 +2,12 @@
 if (location.pathname.match('/cgi-bin/koha/members/memberentry.pl')) {
 $(()=>{
 
-    // add University ID patron attribute to the "quick add" form
-    // copied from existing form, changed IDs so they're unique (added "_qa")
-    let html = `<li>
-                    <label for="patron_attr_3_qa">University ID: </label>
-                    <input type="text" size="20" id="patron_attr_3_qa" name="patron_attr_3">
-                    <input type="hidden" id="patron_attr_3_code_qa" name="patron_attr_3_code" value="UNIVID" class="noEnterSubmit">
-                </li>`
-    $('#quick_add_list').append(html)
-
     // make University ID required for the patron categories below
     let types = ['FACULTY', 'GRAD', 'STAFF', 'UNDERGRAD']
     let type = new URLSearchParams(location.search).get('categorycode')
     if (types.includes(type)) {
-        // also target the quick add form input we just added above
-        $('label[for="patron_attr_3"], label[for="patron_attr_3_qa"]').addClass('required')
-        $('#patron_attr_3, #patron_attr_3_qa').prop('required', true)
+        $('label[for="patron_attr_3"]').addClass('required')
+        $('#patron_attr_3').prop('required', true)
             .addClass('noEnterSubmit')
             .after('<span class="required">Required</span>')
     }
