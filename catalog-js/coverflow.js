d5b10c03712fab9af74b4d91b4e818b8482ea23f
phette23 <phette23@gmail.com>
2018-03-19 10:32:36 -0700

    fix HTTP coverflow images

--- catalog-js/coverflow.js
+++ catalog-js/coverflow.js
@@ -15,9 +15,11 @@ if (location.pathname.match('/cgi-bin/koha/opac-main.pl') || location.pathname =
                         function() {
                             if (this.naturalHeight == 1) {
                                 $(this).attr("src",
-                                    "http://media.bywatersolutions.com/Model/NoImage.png"
+                                    "https://media.bywatersolutions.com/Model/NoImage.png"
                                 );
                             }
+                            // fix all the HTTP Amazon images
+                            $(this).attr('src', (idx, val) => val.replace('://images.', 's://images-na.ssl-images-'));
                         });
                     var opt = {
                         'items': '.item',
