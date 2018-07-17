74e3ce15133069dfa54c7f830704d8d13891c031
phette23 <phette23@gmail.com>
2018-07-17 12:49:55 -0700

    prevent course reserve deletion if there are items

---
+++ admin-js/course-reserves.js
@@ -0,0 +1,15 @@
+// If you delete a course reserve without first removing all items from it
+// the item fields get all messed up, this stops us from doing that.
+if (location.pathname.match('/cgi-bin/koha/course_reserves/course-details.pl')) {
+    let num_items = $('#course_reserves_table tbody tr').length
+    if (num_items > 0) {
+        // disable Delete Course button
+        let msg = 'Remove all items from the course before deleting it.'
+        $('#delete_course')
+            .attr('disabled', true).attr('title', msg)
+            .off('click').on('click', () => {
+                alert(msg)
+                return false
+            })
+    }
+}
