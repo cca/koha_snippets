20418b7bea85b46ed8d3c373b211f0821ade4b1b
phette23 <phette23@gmail.com>
2018-08-28 11:44:27 -0700

    set up login redirect for _multiple_ holds at once
    turns out to have slightly different URL structure :angry:

--- catalog-js/login-redirects.js
+++ catalog-js/login-redirects.js
@@ -2,7 +2,7 @@
 // this script uses session storage to remember what's going on & redirect
 // users back to the appropriate place for these actions:
 // 1) back to opac-reserve.pl?biblionumber=N when placing hold
-// 2) back to opac-suggestions.pl?op=add when making purchase suggestion
+// 2) back to opac-suggestionstorage.pl?op=add when making purchase suggestion
 // 3) back to opac-request-article.pl?biblionumber=N when requesting an article
 
 // this is a little weird: we assume we're on the login screen if we see the
@@ -10,44 +10,54 @@
 // document (not even <html> element)
 var onLoginScreen = !!document.childNodes[1].textContent.match('opac-auth.tt')
     , path = location.pathname
+    , search = location.search
+    , storage = window.sessionStorage
+    , bibs
     , bib
 
 // 1) record bib number when we're trying to place a hold but not logged in
 if (onLoginScreen && path.match('/cgi-bin/koha/opac-reserve.pl')) {
     // we always clear storage first in case multiple actions build up
     // e.g. first they place hold, sign out, request article in same session
-    sessionStorage.clear()
-    bib = location.search.match(/biblionumber=(\d+)/)[1]
-    sessionStorage.setItem('cca_bib_hold', bib)
+    storage.clear()
+    bib = search.match(/biblionumber=(\d+)/) && search.match(/biblionumber=(\d+)/)[1]
+    // URL structure is slightly different for multiple holds, biblionumbers
+    // are listed in one parameter and forward-slash separated
+    bibs = search.match(/biblionumbers=([\d/]+)/) && search.match(/biblionumbers=([\d/]+)/)[1]
+    if (bib) storage.setItem('cca_bib_hold', bib)
+    if (bibs) storage.setItem('cca_bib_holds', bibs)
 }
-if (path.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.cca_bib_hold) {
-    bib = sessionStorage.cca_bib_hold
+else if (path.match('/cgi-bin/koha/opac-user.pl') && (storage.cca_bib_hold || storage.cca_bib_holds)) {
+    bib = storage.cca_bib_hold
+    bibs = storage.cca_bib_holds
     // clear storage, go to appropriate reserve page
-    sessionStorage.removeItem('cca_bib_hold')
-    location = '/cgi-bin/koha/opac-reserve.pl?biblionumber=' + bib
+    storage.removeItem('cca_bib_hold')
+    storage.removeItem('cca_bib_holds')
+    if (bib) location = '/cgi-bin/koha/opac-reserve.pl?biblionumber=' + bib
+    if (bibs) location = '/cgi-bin/koha/opac-reserve.pl?biblionumbers=' + bibs
 }
 
-// 2) note that we're trying to make a purchase suggestion but we're not logged in
-// note we need to use href because we want to match query string too
-if (onLoginScreen && location.href.match(/\/cgi-bin\/koha\/opac-suggestions.pl\?op=add/)) {
-    sessionStorage.clear()
-    sessionStorage.setItem('cca_suggestion', true)
+// 2) we're trying to make a purchase suggestion but we're not logged in
+// note we need to use location.href because we want to match query string too
+if (onLoginScreen && location.href.match(/\/cgi-bin\/koha\/opac-suggestionstorage.pl\?op=add/)) {
+    storage.clear()
+    storage.setItem('cca_suggestion', true)
 }
-if (path.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.cca_suggestion) {
+else if (path.match('/cgi-bin/koha/opac-user.pl') && storage.cca_suggestion) {
     // clear storage, go to purchase suggestions form
-    sessionStorage.removeItem('cca_suggestion')
-    location = '/cgi-bin/koha/opac-suggestions.pl?op=add'
+    storage.removeItem('cca_suggestion')
+    location = '/cgi-bin/koha/opac-suggestionstorage.pl?op=add'
 }
 
 // 3) we're trying to make an article request but we're not logged in
 if (onLoginScreen && path.match('/cgi-bin/koha/opac-request-article.pl')) {
-    sessionStorage.clear()
-    bib = location.search.match(/biblionumber=(\d+)/)[1]
-    sessionStorage.setItem('cca_article_request', bib)
+    storage.clear()
+    bib = search.match(/biblionumber=(\d+)/)[1]
+    storage.setItem('cca_article_request', bib)
 }
-if (path.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.cca_article_request) {
-    bib = sessionStorage.cca_article_request
+else if (path.match('/cgi-bin/koha/opac-user.pl') && storage.cca_article_request) {
+    bib = storage.cca_article_request
     // clear storage, go to appropriate article requests page
-    sessionStorage.removeItem('cca_article_request')
+    storage.removeItem('cca_article_request')
     location = '/cgi-bin/koha/opac-request-article.pl?biblionumber=' + bib
 }
