71878932acd21ef79626838fd3959c2c84b95638
phette23 <phette23@gmail.com>
2018-09-17 11:55:50 -0700

    course reserves - fix RemoveInstructor bug
    special chars in barcodes cause an error

--- admin-js/course-reserves.js
+++ admin-js/course-reserves.js
@@ -13,3 +13,19 @@ if (location.pathname.match('/cgi-bin/koha/course_reserves/course-details.pl')) 
             })
     }
 }
+
+if (location.pathname.match('/cgi-bin/koha/course_reserves/course.pl')) {
+    // fix bug with barcodes that have CSS special characters in them
+    // they are passed un-sanitized as a jQuery selector so special chars break
+    // we'll just redo this functionality from scratch since other approaches
+    // (editing the barcode value, redefining RemoveInstructor()) didn't work
+    $('#instructors div').each((idx, instructor) => {
+        instructor.id = instructor.id.replace(/([$.:\[\],=@])/g, "\\$1")
+    })
+    $(document).on('click', '.removeInstructor', (ev) => {
+        $(ev.target).parent().remove()
+        if (!$('#instructors').html()) {
+            $('#course_instructors').hide(800);
+        }
+    })
+}
