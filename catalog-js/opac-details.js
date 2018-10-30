deeee1ad4cc37c77f9d57b75b74d92122eb48e1c
phette23 <phette23@gmail.com>
2018-10-30 12:46:42 -0700

    work around another series link bug, Koha bug 21589

--- catalog-js/opac-details.js
+++ catalog-js/opac-details.js
@@ -47,11 +47,18 @@ if (location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
         // execute function we just redefined
         KOHA.LocalCover.GetCoverFromBibnumber(true)
 
-        // fix 830$a series links ending in semicolons, see bug #14716
+        // fix two issues related to 830 series links
         $('.results_summary.series a').each(function(){
-            // URI-encoded semicolon
-            let href = $(this).attr('href').replace(/;/g, '%3B')
-            $(this).attr('href', href)
+            let el = $(this)
+            // fix 830$a series links ending in semicolons, see bug #14716
+            let href = el.attr('href').replace(/;/g, '%3B')
+            el.attr('href', href)
+            // fix 490$a linked to 830 links, bug #21589
+            // move query from author index to series
+            if (href.match(/q=se,phr:%22%22&q=au:%22/)) {
+                let query = href.match(/q=au:%22(.*)%22/)[1]
+                el.attr('href', `/cgi-bin/koha/opac-search.pl?q=se,phr:%22${query}%22`)
+            }
         })
 
         // all manner of links with "&" in the text are broken
