e8581ea29bb3478c916e269d250c27f611d90008
phette23 <phette23@gmail.com>
2019-03-21 17:17:01 -0700

    another take at getting timing of cover images right

--- catalog-js/https-upgrade.js
+++ catalog-js/https-upgrade.js
@@ -4,17 +4,22 @@
 // is different.
 $(()=>{
     if (location.protocol === 'https:') {
-        // HTTP image, mixed content
-        $('img[src^="http://ecx.images-amazon.com"]').each((idx, img) => {
-            let new_src = img.src.replace('http://ecx.images-amazon.com', '//images-na.ssl-images-amazon.com')
-            fetch(new_src)
-                .then((response)=> {
-                    // successful request implies image is available
-                    if (response.ok) {
-                        img.src = new_src
-                        return Promise.resolve('silence console error')
-                    }
-                })
-        })
+        let swap = function () {
+            // HTTP image, mixed content
+            $('img[src^="http://ecx.images-amazon.com"]').each((idx, img) => {
+                let new_src = img.src.replace('http://ecx.images-amazon.com', '//images-na.ssl-images-amazon.com')
+                fetch(new_src)
+                    .then((response)=> {
+                        // successful request implies image is available
+                        if (response.ok) {
+                            img.src = new_src
+                            return Promise.resolve('silence console error')
+                        }
+                    })
+            })
+        }
+
+        // run a second after page load, hopefully we have COCE data by now
+        setTimeout(swap, 1000)
     }
 })
