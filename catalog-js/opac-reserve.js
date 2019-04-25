e53771ee363d43d41c69509f1bee3004e42083fa
phette23 <phette23@gmail.com>
2019-04-25 12:26:48 -0700

    opac-reserve - hide 'hold not needed by date'
    much simpler code, old method was not working & didn't reduce cognitive burden on users

--- catalog-js/opac-reserve.js
+++ catalog-js/opac-reserve.js
@@ -7,20 +7,7 @@ if (location.pathname.match('/cgi-bin/koha/opac-reserve.pl')) {
         $('.holdrow').append('<div class="alert"><b>Note that this item is for in-library use only.</b></div>')
     }
 
-    // some people put the "hold not needed date" immediately after the
-    // "hold starts on" date — we recommend at least a week buffer here
-    $('.holddatefrom, .holddateto').blur(()=>{
-        let d1 = new Date($('.holddatefrom').val()).getTime()
-        let d2 = new Date($('.holddateto').val()).getTime()
-        // difference in days
-        let diff = parseInt((d2 - d1) / (24 * 3600 * 1000))
-        let html = '<li class="js-holddatewarning text-error">We recommend at least seven days in between the hold start date and "not needed by" date.</li>'
-
-        // show a warning if there isn't one already
-        if (diff < 7 && !$('.js-holddatewarning').length) {
-            $('.holddateto').parent().append(html)
-        } else if (diff >= 7) {
-            $('.js-holddatewarning').remove()
-        }
-    })
+    // hide "hold not needed after" datepicker
+    // it generates more confusion than it adds utility
+    $('.holddateto').parent().hide()
 }
