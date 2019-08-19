e2cd95f544395184a150014b1e4ecaec123201e5
phette23 <phette23@gmail.com>
2019-08-19 13:34:33 -0700

    save to lists for signed out users, closes #12

---
+++ catalog-js/save-to-lists.js
@@ -0,0 +1,25 @@
+// https://github.com/cca/koha_snippets/issues/12
+// "save to my lists" feature for unauthenticated users
+// this feature touches multiple pages & is kept in this one file for sanity
+var storage = window.sessionStorage
+var loggedIn = !!$('.loggedinusername').length
+var s = '#action .addtoshelf'
+
+if (location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
+    if (!loggedIn) {
+        // add link, make it store data & go to user sign in
+        var biblionumber = new URLSearchParams(location.search).get('biblionumber')
+        $('#action li').eq(0).after('<li><a class="addtoshelf" href="/cgi-bin/koha/opac-user.pl">Save to your lists</a></li>')
+        $(s).click((e) => storage.cca_save_to_list = biblionumber)
+    // if we're signed in & have stored data, click the link then clear data
+} else if (loggedIn && storage.cca_save_to_list) {
+        $(s).click()
+        storage.removeItem('cca_save_to_list')
+    }
+}
+// check that we're signed in & that there's stored data, then redirect
+if (location.pathname.match('/cgi-bin/koha/opac-user.pl')) {
+    if (loggedIn && storage.cca_save_to_list) {
+        location = '/cgi-bin/koha/opac-detail.pl?biblionumber=' + storage.cca_save_to_list
+    }
+}
