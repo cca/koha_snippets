aa22002ffb678a0d03b59db455ee9b1d9994d44c
phette23 <phette23@gmail.com>
2019-09-05 10:56:08 -0700

    opac login, fix issue for people w/o accounts
    failed CAS login notice was being hidden plus wasn't informative
    make it not hide and update the text to be more useful

--- catalog-js/login-cas.js
+++ catalog-js/login-cas.js
@@ -12,6 +12,12 @@ $(() => {
 
         // wipe out everything _except alerts_, swap in our custom HTML
         login.remove()
+        // for some reason this warning no longer is an alert...
+        var failedCAS = $('p:contains("Sorry, the CAS login failed")')
+        failedCAS.addClass('alert').html(`Sorry, your CCA login failed. It's likely
+        that you don't have a library account yet or have one under a different
+        username. Please <a href="https://libraries.cca.edu/contact">contact us</a>
+        for assistance.`)
         var alerts = $('#opac-auth .alert')
         $('#opac-auth').html('')
             .append('<br>').append(alerts).append(login)
