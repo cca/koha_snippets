e8e1f0d131c0678af717907542f240b539e91f6e
phette23 <phette23@gmail.com>
2019-09-16 11:39:22 -0700

    admin mainpage - always show article requests

--- admin-js/mainpage.js
+++ admin-js/mainpage.js
@@ -1,37 +1,32 @@
 // modifications to the "main page" — page staff see right after logging in
 if (location.pathname.match('/cgi-bin/koha/mainpage.pl')) {
     $(function(){
-        let article_requests = $('#article_requests_pending a')
-        let suggestions_pending = $('#suggestions_pending a')
+        let suggestions = $('#suggestions_pending a')
 
         // make article requests link go to _all_ libraries
-        article_requests.attr('href', (i, href) => {
+        $('#article_requests_pending a').attr('href', (i, href) => {
             return href + '?branchcode='
         })
 
         // same with acquisitions requests, make them show all libraries
-        suggestions_pending.attr('href', (i, href) => {
+        suggestions.attr('href', (i, href) => {
             return href.replace('#ASKED', '?displayby=STATUS&branchcode=__ANY__#ASKED')
         })
 
         // add new module buttons for pending article/purchase requests
-        let html = ''
-        if (article_requests.length) {
-            html += `<li>
+        $('.biglinks-list').eq(0).append(`<li>
                 <a class="icon_general icon_serials"
-                    href="${article_requests.attr('href')}">'
-                        ${article_requests.text()}
+                    href="/cgi-bin/koha/circ/article-requests.pl?branchcode=">
+                        Article requests
                 </a>
-            </li>`
-        }
-        if (suggestions_pending.length) {
-            html += `<li>
+            </li>`)
+        if (suggestions.length) {
+            $('.biglinks-list').eq(1).append(`<li>
                 <a class="icon_general icon_acquisitions"
-                    href="${suggestions_pending.attr('href')}">
-                        ${suggestions_pending.text()}
+                    href="${suggestions.attr('href')}">
+                        ${suggestions.text()}
                 </a>
-            </li>`
+            </li>`)
         }
-        $('.biglinks-list').eq(1).append(html)
     })
 }
