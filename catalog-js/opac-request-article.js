09711fc493018f0d10c484502b67361eec6e88c4
phette23 <phette23@gmail.com>
2019-12-10 12:13:24 -0800

    19.05 upgrade - remove 2 unneeded hax

--- catalog-js/opac-request-article.js
+++
@@ -1,8 +0,0 @@
-// fields made mandatory in settings only have <label> with a "required" class
-// we need to make their corresponding inputs required, too
-if (path.match('/cgi-bin/koha/opac-request-article.pl')) {
-    $('#place-article-request label.required').each((idx, el) => {
-        let input = $(el).attr('for')
-        $(`#${input}`).prop('required', true)
-    })
-}
