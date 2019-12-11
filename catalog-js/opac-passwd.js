99630b988b0bdefdcf53bdf52a5a837776c159c5
phette23 <phette23@gmail.com>
2019-12-11 15:05:39 -0800

    note about resetting CCA password, closes #17

---
+++ catalog-js/opac-passwd.js
@@ -0,0 +1,8 @@
+// "change your password" page, currently only Alumni have access
+if (path.match('/cgi-bin/koha/opac-passwd.pl')) {
+    let msg = `<br>Note that this page only changes your Library Catalog password.
+        Some Alumni may sign in with their CCA password; this page does not reset that
+        password, instead see <a href="https://accounts.cca.edu/reset_password/">
+        the CCA accounts website</a>.`
+    $('#mainform .alert-info').append(msg)
+}
