a01058a82cf351a68c833ecb2fbb7fa01a265b5e
phette23 <phette23@gmail.com>
2020-02-09 10:53:55 -0800

    disable end of semester note

--- catalog-js/opac-user.js
+++ catalog-js/opac-user.js
@@ -13,34 +13,34 @@ if (path.match('/cgi-bin/koha/opac-user.pl')) {
         $el.html(html.replace('contact the library', replacement))
     })
 
-    let showMessage = () => {
-        // BUG #23968 OPACMySummaryNote does not display, fixed in 19.11
-        if (!$('#opac-my-summary-note').length) {
-            let note = `<h3>Students: Important End-of-Semester Information</h3>
-            <p>Undergraduate and graduate students cannot borrow or renew items past Dec 13 until they have updated their accounts in-person at either campus library. See <a onclick="return ga('send', 'event', 'OPACMySummaryNote', 'blog post link', this.href);" href="https://libraries.cca.edu/news/winter-break-2019-checkouts-and-hours/">the libraries' blog</a> for information on how to update your account, borrowing over winter break, and our complete winter hours.</p>`
-            // if there's already an alert, prepend this to it, otherwise create an alert
-            if ($('.alert').not('#notesaved').length) {
-                $('.alert').not('#notesaved').eq(0).prepend(note)
-            } else {
-                $('#notesaved').before(`<div class="alert alert-warning">${note}</div>`)
-            }
-        }
-    }
-
-    // show note only if user is a student, get patron type via public report
-    let id = $('.loggedinusername').data('borrowernumber')
-    if (id && fetch) {
-        let report = 'https://library.cca.edu/cgi-bin/koha/svc/report?id=353'
-        fetch(`${report}&sql_params=${id}`)
-            .then(resp => resp.json())
-            .then(data => {
-                // response is always an array of arrays: [ [ "ALUMNI" ] ]
-                let category = data[0][0]
-                if (category == 'UNDERGRAD' || category == 'GRAD') showMessage()
-            })
-            .catch((e) => console.error(e))
-    } else {
-        // default to showing the message
-        showMessage()
-    }
+    // let showMessage = () => {
+    //     // BUG #23968 OPACMySummaryNote does not display, fixed in 19.11
+    //     if (!$('#opac-my-summary-note').length) {
+    //         let note = `<h3>Students: Important End-of-Semester Information</h3>
+    //         <p>Undergraduate and graduate students cannot borrow or renew items past Dec 13 until they have updated their accounts in-person at either campus library. See <a onclick="return ga('send', 'event', 'OPACMySummaryNote', 'blog post link', this.href);" href="https://libraries.cca.edu/news/winter-break-2019-checkouts-and-hours/">the libraries' blog</a> for information on how to update your account, borrowing over winter break, and our complete winter hours.</p>`
+    //         // if there's already an alert, prepend this to it, otherwise create an alert
+    //         if ($('.alert').not('#notesaved').length) {
+    //             $('.alert').not('#notesaved').eq(0).prepend(note)
+    //         } else {
+    //             $('#notesaved').before(`<div class="alert alert-warning">${note}</div>`)
+    //         }
+    //     }
+    // }
+    //
+    // // show note only if user is a student, get patron type via public report
+    // let id = $('.loggedinusername').data('borrowernumber')
+    // if (id && fetch) {
+    //     let report = 'https://library.cca.edu/cgi-bin/koha/svc/report?id=353'
+    //     fetch(`${report}&sql_params=${id}`)
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             // response is always an array of arrays: [ [ "ALUMNI" ] ]
+    //             let category = data[0][0]
+    //             if (category == 'UNDERGRAD' || category == 'GRAD') showMessage()
+    //         })
+    //         .catch((e) => console.error(e))
+    // } else {
+    //     // default to showing the message
+    //     showMessage()
+    // }
 }
