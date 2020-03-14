704bff5c1fe41565b4e2e91e15b8b948a610c301
phette23 <phette23@gmail.com>
2020-03-13 17:12:29 -0700

    update patron notice with info about continuing library services

--- catalog-js/opac-user.js
+++ catalog-js/opac-user.js
@@ -13,20 +13,24 @@ if (path.match('/cgi-bin/koha/opac-user.pl')) {
         $el.html(html.replace('contact the library', replacement))
     })
 
-    // let showMessage = () => {
-    //     // BUG #23968 OPACMySummaryNote does not display, fixed in 19.11
-    //     if (!$('#opac-my-summary-note').length) {
-    //         let note = `<h3>Students: Important End-of-Semester Information</h3>
-    //         <p>Undergraduate and graduate students cannot borrow or renew items past Dec 13 until they have updated their accounts in-person at either campus library. See <a onclick="return ga('send', 'event', 'OPACMySummaryNote', 'blog post link', this.href);" href="https://libraries.cca.edu/news/winter-break-2019-checkouts-and-hours/">the libraries' blog</a> for information on how to update your account, borrowing over winter break, and our complete winter hours.</p>`
-    //         // if there's already an alert, prepend this to it, otherwise create an alert
-    //         if ($('.alert').not('#notesaved').length) {
-    //             $('.alert').not('#notesaved').eq(0).prepend(note)
-    //         } else {
-    //             $('#notesaved').before(`<div class="alert alert-warning">${note}</div>`)
-    //         }
-    //     }
-    // }
-    //
+    let showMessage = () => {
+        // BUG #23968 OPACMySummaryNote does not display, fixed in 19.11
+        if (!$('#opac-my-summary-note').length) {
+            let url = 'http://libraries.cca.edu/news/information-regarding-libraries-during-suspension-person-classes'
+            // COVID-19 note
+            let note = `<h3>Library services during suspension of in-person classes</h3>
+            <p>The CCA Libraries will remain open while in-person classes are suspended at CCA but on Spring Break hours (9am - 5pm, Monday through Friday). See <a onclick="return ga('send', 'event', 'OPACMySummaryNote', 'blog post link', this.href);" href="${url}">the libraries' blog</a> for further information.</p>`
+            // if there's already an alert, prepend this to it, otherwise create an alert
+            if ($('.alert').not('#notesaved').length) {
+                $('.alert').not('#notesaved').eq(0).prepend(note)
+            } else {
+                $('#notesaved').before(`<div class="alert alert-warning">${note}</div>`)
+            }
+        }
+    }
+    // show for everyone
+    showMessage()
+
     // // show note only if user is a student, get patron type via public report
     // let id = $('.loggedinusername').data('borrowernumber')
     // if (id && fetch) {
