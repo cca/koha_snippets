64f2c685abdd96b2035e561798b29f48b797e541
phette23 <phette23@gmail.com>
2020-03-17 12:48:23 -0700

    put librarian chat widget on more pages
    all the personal account pages

--- catalog-js/librarian-chat.js
+++ catalog-js/librarian-chat.js
@@ -1,10 +1,22 @@
 // libraryh3lp chat pop-up window, see https://libraryh3lp.com/dashboard/services
-if (path.match('/cgi-bin/koha/opac-search.pl') || path.match('/cgi-bin/koha/opac-user.pl')) {
+let paths = [
+    '/cgi-bin/koha/opac-search.pl',
+    '/cgi-bin/koha/opac-user.pl',
+    '/cgi-bin/koha/opac-account.pl',
+    '/cgi-bin/koha/opac-memberentry.pl',
+    '/cgi-bin/koha/opac-search-history.pl',
+    '/cgi-bin/koha/opac-readingrecord.pl',
+    '/cgi-bin/koha/opac-privacy.pl',
+    '/cgi-bin/koha/opac-suggestions.pl',
+    '/cgi-bin/koha/opac-messaging.pl',
+    '/cgi-bin/koha/opac-shelves.pl?op=list&category=1'
+]
+if (paths.some(page => path.match(page))) {
     $('body').append('<div class="needs-js">')
-    // three second delay
+    // two second delay
     setTimeout(() => {
       var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
       x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?13843";
       var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-    }, 3000)
+    }, 2000)
 }
