48abcb414298e79d3346604a36d4e2770324c4c8
phette23 <phette23@gmail.com>
2020-03-26 15:37:26 -0700

    only show chat if we're available or free to chat

--- catalog-js/librarian-chat.js
+++ catalog-js/librarian-chat.js
@@ -12,11 +12,15 @@ let paths = [
     '/cgi-bin/koha/opac-shelves.pl?op=list&category=1'
 ]
 if (paths.some(page => path.match(page))) {
-    $('body').append('<div class="needs-js">')
-    // two second delay
-    setTimeout(() => {
-      var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-      x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?13843";
-      var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-    }, 2000)
+    // use libraryh3lp Presence API https://dev.libraryh3lp.com/presence.html
+    fetch('https://libraryh3lp.com/presence/jid/cca-libraries-queue/chat.libraryh3lp.com/text')
+        .then(resp => resp.text())
+        .then(status => {
+            if (status == 'available' || status == 'chat') {
+                $('body').append('<div class="needs-js">')
+                var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+                x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?13843";
+                var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+            }
+        })
 }
