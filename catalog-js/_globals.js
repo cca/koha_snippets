7f0cfaa7732fc3c61cdfe14179eebf3516afd572
phette23 <phette23@gmail.com>
2020-06-23 20:55:05 -0700

    fix bug in _globals.js with concatenated scripts

--- catalog-js/_globals.js
+++ catalog-js/_globals.js
@@ -1,2 +1,3 @@
 // shortcut vars because we use these in multiple snippets
-let path = location.pathname
+// this file should end in a semicolon or else ga.js turns into a function call
+let path = location.pathname;
