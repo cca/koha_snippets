fd483c5e4ccab261a870572ec0d5705badc17ee9
phette23 <phette23@gmail.com>
2021-01-12 12:41:52 -0800

    finish save to lists improvement
    redirect back to search instead of the bib being saved

--- catalog-js/save-to-lists.js
+++ catalog-js/save-to-lists.js
@@ -1,24 +1,22 @@
 // https://github.com/cca/koha_snippets/issues/12
 // "save to my lists" feature for unauthenticated users
 // this feature touches multiple pages & is kept in this one file for sanity
-// @TODO if the Save to List button is clicked on the search results page, then
-// we should return to results page rather than bib detail of the saved item
 let loggedIn = !!$('.loggedinusername').length
-let addToShelfAction = '.addtoshelf'
+ , addToShelfAction = '.addtoshelf'
 
 function addSaveToListBtn(target, wrapper) {
     let html = `<${wrapper}><a class="addtoshelf btn btn-lg btn-link" href="/cgi-bin/koha/opac-user.pl"><i class="fa fa-fw fa-list"></i> Save to your lists</a></${wrapper}>`
 
     // add link, make it store data & go to user sign in
-    var biblionumber = new URLSearchParams(location.search).get('biblionumber')
     $(target).after(html)
     $(addToShelfAction).click(function(event) {
-        if (!biblionumber) {
-            // we're on search page, find bib number from URL in title link
-            let recordLink = $(this).closest('.title_summary').find('a.title')[0].href
-            biblionumber = new URLSearchParams(recordLink.split('?')[1]).get('biblionumber')
+        let bib = new URLSearchParams(location.search).get('biblionumber')
+        if (!bib) {
+            // we're on search page, find bib number from ID of title element
+            bib = $(this).closest('.title_summary').attr('id').split('title_summary_')[1]
+            sessionStorage.setItem('cca_search_url', location.toString())
         }
-        sessionStorage.cca_save_to_list = biblionumber
+        sessionStorage.cca_save_to_list = bib
     })
 }
 
@@ -31,13 +29,28 @@ if (!loggedIn) {
         addSaveToListBtn('.actions-menu span.actions:last-child', 'span')
     }
 } else if (sessionStorage.cca_save_to_list) {
-    // we are logged in _and_ have data, where are we in the process?
-    if (path.match('/cgi-bin/koha/opac-user.pl')) {
-        // step 1) user page, so we just signed in, redirect to bib
-        location = '/cgi-bin/koha/opac-detail.pl?biblionumber=' + sessionStorage.cca_save_to_list
-    } else if (path.match('/cgi-bin/koha/opac-detail.pl')) {
-        // step 2) click the "save to lists" link and then clear data
-        $(addToShelfAction).click()
-        sessionStorage.removeItem('cca_save_to_list')
+    let bib = sessionStorage.cca_save_to_list
+    // we are logged in _and_ have data but were we on a search or details page?
+    if (sessionStorage.cca_search_url) {
+        // we started on a search page
+        if (path.match('/cgi-bin/koha/opac-user.pl')) {
+            // step 1) user page, so we just signed in, redirect to saved search
+            location = sessionStorage.cca_search_url
+        } else if (path.match('/cgi-bin/koha/opac-search.pl')) {
+            // step 2) find bib, click "save to lists" action then clear data
+            $('#title_summary_' + bib).find('.actions [href^="/cgi-bin/koha/opac-addbybiblionumber.pl"]').click()
+            sessionStorage.removeItem('cca_save_to_list')
+            sessionStorage.removeItem('cca_search_url')
+        }
+    } else {
+        // we start on a details page, simpler process
+        if (path.match('/cgi-bin/koha/opac-user.pl')) {
+            // step 1) user page, so we just signed in, redirect to bib
+            location = '/cgi-bin/koha/opac-detail.pl?biblionumber=' + bib
+        } else if (path.match('/cgi-bin/koha/opac-detail.pl')) {
+            // step 2) click the "save to lists" link then clear data
+            $(addToShelfAction).click()
+            sessionStorage.removeItem('cca_save_to_list')
+        }
     }
 }
