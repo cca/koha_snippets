46beedbfc2c91a24efd863498dfd0a98d2aa3de6
Eric Phetteplace <phette23@gmail.com>
2021-02-19 10:18:57 -0800

    move Reports home pg edit to HTML setting

--- admin-js/reports-home.js
+++
@@ -1,7 +0,0 @@
-// add links to the reports home page
-if (path.match('/cgi-bin/koha/reports/reports-home.pl')) {
-    $(() => {
-        // not easy to select the "Top lists" list
-        $('.yui-u').last().find('ul').first().append('<li><a href="/cgi-bin/koha/reports/guided_reports.pl?reports=218&phase=Run%20this%20report">New Books Shelf</a></li>')
-    })
-}
