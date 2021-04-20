8b394108e2cf9948de2ec1a04dfd0d911b0d4ca7
Eric Phetteplace <phette23@gmail.com>
2021-04-20 15:58:40 -0700

    eslint - fix mixed spaces & tabs

--- admin-js/inventory.js
+++ admin-js/inventory.js
@@ -3,40 +3,48 @@
 // in a separate file
 if (path === '/cgi-bin/koha/tools/inventory.pl') {
 
-	var fixLCCNsort = function() {
-		let pagination = $('select[name="inventoryt_length"]').val()
-		if (pagination !== "-1") {
-			alert('You must set "Show ____ entries" to "All" before clicking this.')
-			return false
-		}
-		// apply LCC sort to datatable
-		let loc = new locCallClass()
-		let it = $('#inventoryt')
-		it.find('tr').each( (idx, el) => {
-			let lccn = $(el).find('td').eq(2)
-			lccn.attr( 'data-order', loc.returnNormLcCall( lccn.text() ) )
-		})
-		// now that we have data-order values in HTML, destroy & redraw dataTable
-		it.DataTable().destroy()
-		// copied from Koha page source, overwrite global inventorydt var
-		inventorydt = it.dataTable($.extend(true, {}, dataTablesDefaults, {
-		    'sPaginationType': 'full_numbers',
-	        "aoColumnDefs": [
-		            { "bSortable": false, "bSearchable": false, "aTargets": [ 0 ] },
-		        ],
-	        "aaSorting": [[ 2, "asc" ]],
-		    'fnDrawCallback': function() {
-		        $('.openWin').bind('click',function(e){
-		            e.preventDefault();
-		            openWindow(this.href,'marcview',800,600);
-		        });
-		    }
-		}));
-	}
+    var fixLCCNsort = function() {
+        let pagination = $('select[name="inventoryt_length"]').val()
+        if (pagination !== "-1") {
+            alert(
+                'You must set "Show ____ entries" to "All" before clicking this.'
+            )
+            return false
+        }
+        // apply LCC sort to datatable
+        let loc = new locCallClass()
+        let it = $('#inventoryt')
+        it.find('tr').each((idx, el) => {
+                let lccn = $(el).find('td').eq(2)
+                lccn.attr('data-order', loc.returnNormLcCall(lccn.text()))
+            })
+            // now that we have data-order values in HTML, destroy & redraw dataTable
+        it.DataTable().destroy()
+            // copied from Koha page source, overwrite global inventorydt var
+        inventorydt = it.dataTable($.extend(true, {}, dataTablesDefaults, {
+            'sPaginationType': 'full_numbers',
+            "aoColumnDefs": [{
+                "bSortable": false,
+                "bSearchable": false,
+                "aTargets": [0]
+            }, ],
+            "aaSorting": [
+                [2, "asc"]
+            ],
+            'fnDrawCallback': function() {
+                $('.openWin').bind('click', function(e) {
+                    e.preventDefault();
+                    openWindow(this.href,
+                        'marcview', 800, 600);
+                });
+            }
+        }));
+    }
 
-	$(document).ready(()=> {
-		let html = '&nbsp;<a class="btn btn-primary js-cn-sort-fix">Fix Call Number Sorting</a>'
-		$('.clearall').eq(0).after(html)
-		$('.js-cn-sort-fix').on('click', fixLCCNsort)
-	})
+    $(document).ready(() => {
+        let html =
+            '&nbsp;<a class="btn btn-primary js-cn-sort-fix">Fix Call Number Sorting</a>'
+        $('.clearall').eq(0).after(html)
+        $('.js-cn-sort-fix').on('click', fixLCCNsort)
+    })
 }
