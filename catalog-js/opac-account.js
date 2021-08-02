7c90c3feb47889689517b7b2a38b794a513ea118
Eric Phetteplace <phette23@gmail.com>
2021-08-02 15:05:50 -0700

    add explanatory line under opac-account btn

--- catalog-js/opac-account.js
+++ catalog-js/opac-account.js
@@ -2,7 +2,7 @@
 if (path.match('/cgi-bin/koha/opac-account.pl')) {
     // only show if the user has fine
     if (parseFloat($('.sum').eq(1).text())) {
-        let html = '<p><a href="https://secure.touchnet.net/C20080_ustores/web/product_detail.jsp?PRODUCTID=210&SINGLESTORE=true" class="btn btn-warning" style="color:white;" target="_blank">Pay fines online</p>'
+        let html = '<p><a href="https://secure.touchnet.net/C20080_ustores/web/product_detail.jsp?PRODUCTID=210&SINGLESTORE=true" class="btn btn-warning" style="color:white;" target="_blank">Pay fines online</a></p><p>Please allow up to 24 hours for online payments to be processed in your library account.</p>'
         $('#useraccount h3').first().after(html)
     }
 }
