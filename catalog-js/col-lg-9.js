3a92107c59d8e2ec2cc582ce6f918d8fda1ee43c
Eric Phetteplace <phette23@gmail.com>
2021-09-28 09:14:47 -0700

    rm navbar styles & col-lg-9 bootstrap fix, ref #31

--- catalog-js/col-lg-9.js
+++
@@ -1,8 +0,0 @@
-// fix formatting bug in 20.05.09, wrong Boostrap class is used
-// https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=28021
-if ($('#opac-detail').length) {
-  if ($('meta[name="generator"]').attr('content').slice(0, 10) === 'Koha 20.05') {
-         $('#catalogue_detail_biblio').parent().addClass('span9').removeClass('col-lg-9')
-         $('.row').addClass('row-fluid').removeClass('row')
-  }
-}
--- catalog-js/home-search.js
+++ catalog-js/home-search.js
@@ -1,18 +1,7 @@
-// remove old submit btn & search input, replace with larger .input-append one
 // better home page column layout
 
 // run only on home pg (could be at domain root or its own path)
 if (path.match('cgi-bin/koha/opac-main.pl') || path === '/') {
-    $('#searchsubmit').remove()
-    // padding is magic number to make text input match large input-append btn height
-    $('#translControl1').replaceWith(
-        `<div class="input-append">
-            <input type="text" title="Type search term" class="input-xxlarge search-query"
-                style="padding:.78em;" id="translControl1"
-                placeholder="search for books, videos, materials..." name="q">
-                <button type="submit" class="btn btn-primary btn-large">Search</button>
-        </div>`)
-    $('#searchform').addClass('form-search')
     // there are 3 columns on the home page:
     // - span2 with #opacnavbottom (hours)
     // - span7 with #opacmainuserblock (coverflow)
