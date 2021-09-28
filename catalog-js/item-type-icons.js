0024f46d0523e7c086d501c6420410b25ee6c2c7
Eric Phetteplace <phette23@gmail.com>
2021-09-28 13:37:15 -0700

    search - material type icon names changed

--- catalog-js/item-type-icons.js
+++ catalog-js/item-type-icons.js
@@ -2,20 +2,22 @@
 if (path.match('/cgi-bin/koha/opac-search.pl')) {
     let icon_map = {
         'book': 'book.svg',
+        'continuing resource': 'periodical.svg',
+        'film': 'video-40',
+        'kit': 'other.svg',
+        'mixed materials': 'other.svg',
         'other materials': 'other.svg',
+        'picture': 'other.svg',
         'reserve': 'reserved-book.svg',
         'serial': 'periodical.svg',
+        'text': 'book.svg',
         'periodicals': 'periodical.svg',
         'video': 'video-40',
         'visual material': 'video-40'
     }
     let replaceIcon = function (img) {
-        for (let type of Object.keys(icon_map)) {
-            if (img.alt.toLowerCase().match(type)) {
-                img.src = `https://libraries.cca.edu/media/images/${icon_map[type]}.original.png`
-                break
-            }
-        }
+        let type = img.alt.toLowerCase()
+        if (icon_map[type]) return img.src = `https://libraries.cca.edu/media/images/${icon_map[type]}.original.png`
     }
 
     Array.from(document.querySelectorAll('.results_material_type img')).forEach(replaceIcon)
