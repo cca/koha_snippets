3d17dd6d27ab15aa2dc79e45d076ad6716b40242
Eric Phetteplace <phette23@gmail.com>
2021-09-28 10:22:05 -0700

    fix 'more searches' main menu, ref #31

--- catalog-js/more-searches-links.js
+++ catalog-js/more-searches-links.js
@@ -1,6 +1,6 @@
 // this is the gray bar beneath the search box that appears on every page
-$('#moresearches ul')
+$('#moresearches')
     .append('<li><a href="https://libraries.cca.edu/contact">Ask a Librarian</a></li>')
     .append('<li><a href="https://libguides.cca.edu">Research Guides</a></li>')
 // rename "Libraries" page
-$('#library_page a').text('Locations')
+$('#library_page').text('Locations')
