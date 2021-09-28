0842cd9eefa8b2b94c6150250966a9ce66fbc0b4
Eric Phetteplace <phette23@gmail.com>
2021-09-28 13:22:42 -0700

    better search box for bibs with >5 items

--- catalog-js/holdings-table-filter.js
+++ catalog-js/holdings-table-filter.js
@@ -1,5 +1,6 @@
 // add a filter to large holdings tables so users can find the issue/volume
 // they want more easily, very useful for serials with large numbers of items
+// ex: https://library.cca.edu/cgi-bin/koha/opac-detail.pl?biblionumber=2806
 
 // we're on a details page & there are more than 4 rows of holdings (not counting header row)
 if (path === '/cgi-bin/koha/opac-detail.pl' && $('#holdingst tr').length > 5) {
@@ -14,11 +15,12 @@ if (path === '/cgi-bin/koha/opac-detail.pl' && $('#holdingst tr').length > 5) {
             "sDom": "ft"
         }));
         // customize filter appearance a bit
-        $('.dataTables_filter').find('label').css('font-weight', 'bold')
+        $('.dataTables_filter').find('label').wrap('<div class="form-inline" style="margin-bottom:16px;"></div>').css('font-weight', 'bold')
             .find('input').css({
                 'margin-left': '1em',
                 'width': '20em'
             })
             .attr('placeholder', 'for a particular volume or issue…')
+            .addClass('form-control')
     })
 }
