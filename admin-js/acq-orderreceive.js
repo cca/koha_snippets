2150fd2fe27f084f5d5c51c0f838de1c6ada6807
Eric Phetteplace <phette23@gmail.com>
2021-09-28 15:47:42 -0700

    acquisitions - make recieving titles more efficient, ref #31
    check off the 'recieved?' checkbox which stopped working
    copy the RRP into the Actual Price field

--- admin-js/acq-orderreceive.js
+++ admin-js/acq-orderreceive.js
@@ -1,9 +1,15 @@
-// check the "Received?" box by default, saves us a click
-// only run on the "receive items" path
+// on the Acquisitions "receive items" page
+// check the "Received?" box by default & fill in actual price
 if (path === '/cgi-bin/koha/acqui/orderreceive.pl') {
-    // run after document load so checkbox is in the DOM
-    $(function(){
-        // no good hook for selector
-        $('.yui-u.first table input[type="checkbox"]').prop('checked', true)
+    // run after document load
+    $(()=>{
+        // almost no classees or other good selector hooks on this page
+        $('table input[type="checkbox"]').prop('checked', true)
+        // copy RRP into "Actual cost" field. The RRP HTML looks like:
+        //   <li><label for="rrp">Retail price: </label>
+        //   5.99<span class="hint">(adjusted for USD,tax exclusive)</span></li>
+        // so we need to get the next text node sibling, FML
+        let rrp = $('label[for="rrp"]').length && $('label[for="rrp"]')[0].nextSibling && $('label[for="rrp"]')[0].nextSibling.textContent.trim()
+        if (rrp) $('#unitprice').val(rrp)
     })
 }
--- catalog-js/more-searches-links.js
+++ catalog-js/more-searches-links.js
@@ -1,4 +1,4 @@
-// this is the gray bar beneath the search box that appears on every page
+// this is the menu beneath the search box that appears on every page
 $('#moresearches')
     .append('<li><a href="https://libraries.cca.edu/contact">Ask a Librarian</a></li>')
     .append('<li><a href="https://libguides.cca.edu">Research Guides</a></li>')
