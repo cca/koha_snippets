a947e69aad5c94d266b29df5d8995b3bb0e88e9e
Eric Phetteplace <phette23@gmail.com>
2021-10-27 10:30:43 -0700

    admin - fix purchase suggestions link in main menu

--- admin-js/mainpage.js
+++ admin-js/mainpage.js
@@ -1,26 +1,9 @@
 // modifications to the "main page" — page staff see right after logging in
 if (path.match('/cgi-bin/koha/mainpage.pl')) {
     $(()=>{
-        // make article requests link go to _all_ libraries
-        $('#article_requests_pending a').attr('href', (i, href) => {
-            return href + '?branchcode='
-        })
-
         // add new module buttons for pending article/purchase requests
         let $menu = $('.biglinks-list')
-        $menu.eq(0).append(`<li>
-                <a class="icon_general icon_serials"
-                    href="/cgi-bin/koha/circ/article-requests.pl?branchcode=">
-                    <i class="fa fa-newspaper-o"></i>
-                        Article requests
-                </a>
-            </li>`)
-        $menu.eq(1).append(`<li>
-            <a class="icon_general icon_acquisitions"
-                href="${$('#all_pendingsuggestions').parent().attr('href')}">
-                <i class="fa fa-gift"></i>
-                Purchase Suggestions
-            </a>
-        </li>`)
+        $menu.eq(0).append('<li><a class="icon_general icon_serials" href="/cgi-bin/koha/circ/article-requests.pl?branchcode="><i class="fa fa-newspaper-o"></i>Article requests</a></li>')
+        $menu.eq(1).append('<li><a class="icon_general icon_acquisitions" href="/cgi-bin/koha/suggestion/suggestion.pl?branchcode=__ANY__#ACCEPTED"><i class="fa fa-gift"></i>Purchase Suggestions</a></li>')
     })
 }
