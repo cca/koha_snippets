6fd8e7435ba4ae3b4481a6df645ee711c51f4ca2
Eric Phetteplace <phette23@gmail.com>
2021-11-18 15:36:44 -0800

    fix opac-account button
    UI change from 21.05 broke it, the h3 element is now an h1

--- catalog-js/opac-account.js
+++ catalog-js/opac-account.js
@@ -1,8 +1,9 @@
-// user's "summary" page immediately after logging in
-if (path.match('/cgi-bin/koha/opac-account.pl')) {
-    // only show if the user has fine
+// show on 1) "charges" tab, and 2) "summary" if there's a secondary "charges" tab
+if (path.match('/cgi-bin/koha/opac-account.pl') || (path.match('/cgi-bin/koha/opac-user.pl') && $('#opac-user-fines').length)) {
+    let html = '<p><a href="https://secure.touchnet.net/C20080_ustores/web/product_detail.jsp?PRODUCTID=210&SINGLESTORE=true" class="btn btn-warning" style="color:white;" target="_blank">Pay fines online</a></p><p>Please allow up to 24 hours for online payments to be processed in your library account.</p>'
+    // only show on charges tab if the user has fine
     if (parseFloat($('.sum').eq(1).text())) {
-        let html = '<p><a href="https://secure.touchnet.net/C20080_ustores/web/product_detail.jsp?PRODUCTID=210&SINGLESTORE=true" class="btn btn-warning" style="color:white;" target="_blank">Pay fines online</a></p><p>Please allow up to 24 hours for online payments to be processed in your library account.</p>'
-        $('#useraccount h3').first().after(html)
+        $('#useraccount h1').first().after(html)
     }
+    $('#opac-user-fines').prepend(html)
 }
