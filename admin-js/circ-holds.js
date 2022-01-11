7dd74fa57a4df7f3be7834f4023c786088ff1255
Eric Phetteplace <phette23@gmail.com>
2022-01-11 10:10:04 -0800

    admin - fix bug with placing hold for one library from another

--- admin-js/circ-holds.js
+++ admin-js/circ-holds.js
@@ -4,22 +4,24 @@ if (path.match('/cgi-bin/koha/reserve/request.pl')) {
         // uncheck "hold next avail. item" on holds page
         // we don't want people placing bib-level holds by accident
         $('#requestany').prop('checked', false)
-        // watch for changes to pickup location
-        // & update allowed pickup locations to match
+        // watch changes to pickup location & make allowed pickup locations match
         // NOTE: we will definitely have to revist this on next upgrade
+        let update_location = () => {
+            let branch = $('#pickup').val()
+            let locations = $('.pickup_locations')
+            // first, unselect everything, then select the matching branch
+            locations.find('option').prop("selected", false)
+            let opt = locations.find(`option[value="${branch}"]`).prop("selected", true)
+            if (!opt.length) {
+                // pickup branch was not in the list so we add it
+                locations.append(`<option value="${branch}" selected="selected">${branch}</option>`)
+            }
+        }
         if ($('#requestspecific').length && $('.pickup_locations').length) {
             // hide pickup location columns as we'll auto update them
             $('#requestspecific').find('th:last-child, td:last-child').hide()
-            $('#pickup').on('change', (ev) => {
-                let branch = $('#pickup').val()
-                $('.pickup_locations').each(function(ev){
-                    let opt = $(this).find(`option[value="${branch}"]`).prop("selected", true)
-                    if (!opt.length) {
-                        // pickup branch is not in the list so we add it
-                        $(this).append(`<option value="${branch}" selected="selected">${branch}</option>`)
-                    }
-                })
-            })
+            update_location()
+            $('#pickup').on('change', (ev) => update_location())
         }
     })
 }
