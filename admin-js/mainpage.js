f2227bc346b9736ba42069b05faf087fb7c03817
phette23 <phette23@gmail.com>
2022-07-25 09:03:20 -0700

    fix icon spacing on admin mainpage

--- admin-js/mainpage.js
+++ admin-js/mainpage.js
@@ -3,7 +3,7 @@ if (path.match('/cgi-bin/koha/mainpage.pl')) {
     $(()=>{
         // add new module buttons for pending article/purchase requests
         let $menu = $('.biglinks-list')
-        $menu.eq(0).append('<li><a class="icon_general icon_serials" href="/cgi-bin/koha/circ/article-requests.pl?branchcode="><i class="fa fa-newspaper-o"></i>Article requests</a></li>')
-        $menu.eq(1).append('<li><a class="icon_general icon_acquisitions" href="/cgi-bin/koha/suggestion/suggestion.pl?branchcode=__ANY__#ACCEPTED"><i class="fa fa-gift"></i>Purchase Suggestions</a></li>')
+        $menu.eq(0).append('<li><a class="icon_general icon_serials" href="/cgi-bin/koha/circ/article-requests.pl?branchcode="><i class="fa fa-fw fa-newspaper-o"></i>Article requests</a></li>')
+        $menu.eq(1).append('<li><a class="icon_general icon_acquisitions" href="/cgi-bin/koha/suggestion/suggestion.pl?branchcode=__ANY__#ACCEPTED"><i class="fa fa-fw fa-gift"></i>Purchase Suggestions</a></li>')
     })
 }
