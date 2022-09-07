7ab9033036104e12720698309cca406a6a592bd8
phette23 <phette23@gmail.com>
2022-09-07 14:27:42 -0700

    catalog holds improvements, closes #35
    simplify the holds UI by removing the 'no longer needed by'
    datepicker that just confused users and hide the extra options
    for titles where there's only one item, so the table of items
    isn't useful

---
+++ catalog-js/holds.js
@@ -0,0 +1,18 @@
+if (path.match('/cgi-bin/koha/opac-reserve.pl')) {
+    // changes per Paul's request on 2022-09-07
+    // https://ccaweb.slack.com/archives/C01V8JTGRD2/p1662570492099079
+
+    // remove the "not needed by" form input, it's more confusing than helpful
+    $('input[name^="expiration_date"]').parent().hide()
+
+    // Toggle hold options to collapsed, not present on initial page load so we
+    // have to wait & monitor. Hide only if there's just 1 holdable item.
+    if ($('tr.holdable').length === 1) {
+        let int = setInterval(() => {
+            if ($('.toggle-hold-options').length) {
+                $('.toggle-hold-options').eq(0).click()
+                clearInterval(int)
+            }
+        }, 500)
+    }
+}
