f3deb6ff394c00418241430d9e4cd74aa67fd521
phette23 <phette23@gmail.com>
2022-09-13 12:22:46 -0700

    fix admin holds on LUOs, SF wasn't defaulting to allowed pickup location

--- admin-js/circ-holds.js
+++ admin-js/circ-holds.js
@@ -4,24 +4,21 @@ if (path.match('/cgi-bin/koha/reserve/request.pl')) {
         // uncheck "hold next avail. item" on holds page
         // we don't want people placing bib-level holds by accident
         $('#requestany').prop('checked', false)
-        // watch changes to pickup location & make allowed pickup locations match
-        // NOTE: we will definitely have to revist this on next upgrade
-        let update_location = () => {
-            let branch = $('#pickup').val()
-            let locations = $('.pickup_locations')
-            // first, unselect everything, then select the matching branch
-            locations.find('option').prop("selected", false)
-            let opt = locations.find(`option[value="${branch}"]`).prop("selected", true)
-            if (!opt.length) {
-                // pickup branch was not in the list so we add it
-                locations.append(`<option value="${branch}" selected="selected">${branch}</option>`)
-            }
-        }
-        if ($('#requestspecific').length && $('.pickup_locations').length) {
-            // hide pickup location columns as we'll auto update them
-            $('#requestspecific').find('th:last-child, td:last-child').hide()
-            update_location()
-            $('#pickup').on('change', (ev) => update_location())
+
+        // LUO items do not auto-select the SF pickup branch in the "allowed
+        // pickup locations" menu
+        let locations = $('.pickup_locations')
+        if ($('#requestspecific').length && locations.length) {
+            // wait for select2 menu to be initialized & check that there are
+            // no options already
+            let int = setInterval(()=>{
+                if (locations.hasClass('select2-hidden-accessible')) {
+                    if (!locations.find('option').length) {
+                        locations.append('<option value="SF" selected="selected">San Francisco</option>')
+                    }
+                    clearInterval(int)
+                }
+            }, 500)
         }
     })
 }
