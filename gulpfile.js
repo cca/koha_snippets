7aba4f3782ca14a49c07ba2476b4e1fb1474363f
phette23 <phette23@gmail.com>
2023-02-24 09:58:38 -0800

    fix gulp-sass, forces you to pass compiler to itself now

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,10 @@ const eslint = require('gulp-eslint')
 const iife = require('gulp-iife')
 const insert = require('gulp-insert')
 const rename = require('gulp-rename')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('sass'))
 const sasslint = require('gulp-sass-lint');
 const uglify = require('gulp-uglify')
 
-sass.compiler = require('sass')
 const sassOpts = {
     outputStyle: 'compressed'
 }
