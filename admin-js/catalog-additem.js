3a73ae14e05ffb0fe0b3e6cd5b3b59bb06c3bb54
phette23 <phette23@gmail.com>
2023-02-24 09:54:32 -0800

    fix JS error, Koha uses flatpickr & not jQuery datepicker library now

--- admin-js/catalog-additem.js
+++ admin-js/catalog-additem.js
@@ -2,11 +2,8 @@
 if (path.match('/cgi-bin/koha/cataloguing/additem.pl')) {
     $( // run a second after DOM load, hopefully form is present
         setTimeout(
-            function(){
-                $('#subfield952w input').first().datepicker({
-                    // ISO-8601 aw yeah
-                    dateFormat: 'yy-mm-dd'
-                })
+            function () {
+                flatpickr('#subfield952w input[name = "items.replacementpricedate"]', {})
             }
         , 1200)
     )
