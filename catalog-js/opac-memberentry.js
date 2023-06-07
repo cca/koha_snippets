a755a1648bbd8fd2d9d0b7e33eba26139cd9f00c
phette23 <phette23@gmail.com>
2023-06-07 11:44:44 -0700

    don't let patrons choose 'other phone' or fax for main contact method

--- catalog-js/opac-memberentry.js
+++ catalog-js/opac-memberentry.js
@@ -3,4 +3,7 @@ if (path.match('/cgi-bin/koha/opac-memberentry.pl')) {
     // only allow SF to be home branch
     $('#borrower_branchcode option[value="SF"]').prop('selected', true)
     $('#borrower_branchcode').prop('disabled', true)
+    // we do not show "other phone" (mobile) or fax to patrons
+    // so we remove them from the "main contact method" dropdown
+    $('#borrower_primary_contact_method').find('option[value="mobile"], option[value="fax"]').remove()
 }
