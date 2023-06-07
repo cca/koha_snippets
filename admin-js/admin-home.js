ac110866ed3715819fdde3f69f6c02be62fb9f8b
phette23 <phette23@gmail.com>
2023-06-07 09:30:21 -0700

    bg jobs are visible on admin home now
    remove customization

--- admin-js/admin-home.js
+++
@@ -1,3 +0,0 @@
-if (path.match('/cgi-bin/koha/admin/admin-home.pl')) {
-    $('.sysprefs').eq(1).find('dl').eq(0).append('<dt><a href="/cgi-bin/koha/admin/background_jobs.pl">Background jobs</a></dt><dd>Status of batch bibliographic record modifications</dd>')
-}
