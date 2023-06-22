e55f71306a737b61cb1f22d99fd9a7234023060e
phette23 <phette23@gmail.com>
2023-06-22 08:34:34 -0700

    admin CAS login - simpler, bootstrap styles

--- admin-js/cas-login.js
+++ admin-js/cas-login.js
@@ -3,10 +3,6 @@ if ($('#main_auth').length && $('#login').length) {
     let heading = $('h4:contains("Cas login")')
     heading.before('<hr>')
     let link = heading.next().find('a')
-    link.addClass('button btn')
-    link.css({
-        'display': 'block',
-        'font-size': '1.4em'
-    })
+    link.addClass('btn btn-primary btn-lg btn-block')
     link.text('Login with CCA Account')
 }
