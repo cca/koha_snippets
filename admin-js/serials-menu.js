59eae4000d83ee9317a3b5aac6c4fc395cd252ba
phette23 <phette23@gmail.com>
2023-06-22 15:05:08 -0700

    improve admin serials menu
    remove outdated OAK menu links
    better jQuery selector for the menu, too

--- admin-js/serials-menu.js
+++ admin-js/serials-menu.js
@@ -3,26 +3,19 @@
 if (path.match('/cgi-bin/koha/serials/')) {
     // execute once document has loaded
     $(() => {
-        // no good style hooks, 18.11 update broke this selector
-        var menu_list = $('.main aside ul').eq(0)
+        var menu_list = $('#navmenulist ul').eq(0)
         // format is: [ link text, URL ]
         var links = [
-            ['OAK at bindery',
-            '/cgi-bin/koha/reports/guided_reports.pl?reports=69&phase=Run%20this%20report']
-            , ['SF at bindery',
+            ['SF at bindery',
             '/cgi-bin/koha/reports/guided_reports.pl?reports=68&phase=Run%20this%20report']
             , ['Item Search for Bindery Items',
             '/cgi-bin/koha/catalogue/itemsearch.pl']
-            , ['OAK Late for Claiming',
-            '/cgi-bin/koha/reports/guided_reports.pl?reports=63&phase=Run%20this%20report']
             , ['SF Late for Claiming',
             '/cgi-bin/koha/reports/guided_reports.pl?reports=64&phase=Run%20this%20report']
             , ['Serials Solutions',
             'https://clientcenter.serialssolutions.com/CC/Login/Default.aspx']
             , ['EBSCONET',
             'https://ebsconet.com']
-            , ['OAK Serials Fund',
-            '/cgi-bin/koha/acqui/ordered.pl?fund=3&fund_code=SER-OAK']
             , ['SF Serials Fund',
             '/cgi-bin/koha/acqui/ordered.pl?fund=2&fund_code=SER-SF']
         ]
