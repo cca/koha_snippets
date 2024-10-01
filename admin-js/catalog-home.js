741f3f21f27c46a0b70ad9987947520d3759ff62
phette23 <phette23@gmail.com>
2024-10-01 13:20:30 -0700

    fix: hide 'new from z39' while it's broken

---
+++ admin-js/catalog-home.js
@@ -0,0 +1,5 @@
+// hide "new from z39.50/SRU" while it's broken
+// https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=37947
+if (path.match('/cgi-bin/koha/cataloguing/cataloging-home.pl')) {
+    $('#z3950search').parent().hide()
+}
