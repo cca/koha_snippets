9ad8afc302e138d3c678c903ccff25689359e12b
phette23 <phette23@gmail.com>
2024-10-01 08:02:59 -0700

    fix: staff CAS login capitalization changed

--- admin-js/cas-login.js
+++ admin-js/cas-login.js
@@ -1,6 +1,6 @@
 // better look for the CAS login link for staff
 if ($('#main_auth').length && $('#login').length) {
-    let heading = $('h4:contains("Cas login")')
+    let heading = $('h4:contains("CAS login")')
     heading.before('<hr>')
     let link = heading.next().find('a')
     link.addClass('btn btn-primary btn-lg btn-block')
