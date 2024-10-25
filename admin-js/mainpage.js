bd5bd77e88a0861823732460f7c81496acf70ae9
phette23 <phette23@gmail.com>
2024-10-25 13:46:02 -0700

    fix: main menu columns same length
    move article requests to 2nd, right-side, column to balance menu

--- admin-js/mainpage.js
+++ admin-js/mainpage.js
@@ -3,7 +3,8 @@ if (location.pathname.match('/cgi-bin/koha/mainpage.pl')) {
     $(()=>{
         // add new module buttons for pending article/purchase requests
         let $menu = $('.biglinks-list')
-        $menu.eq(0).append('<li><a class="icon_general icon_serials" href="/cgi-bin/koha/circ/article-requests.pl?branchcode="><i class="fa fa-fw fa-newspaper"></i>Article requests</a></li>')
-        $menu.eq(1).append('<li><a class="icon_general icon_acquisitions" href="/cgi-bin/koha/suggestion/suggestion.pl?branchcode=__ANY__#ACCEPTED"><i class="fa fa-fw fa-gift"></i>Purchase Suggestions</a></li>')
+        $menu.eq(1)
+            .append('<li><a class="icon_general icon_serials" href="/cgi-bin/koha/circ/article-requests.pl?branchcode="><i class="fa fa-fw fa-newspaper"></i>Article requests</a></li>')
+            .append('<li><a class="icon_general icon_acquisitions" href="/cgi-bin/koha/suggestion/suggestion.pl?branchcode=__ANY__#ACCEPTED"><i class="fa fa-fw fa-gift"></i>Purchase Suggestions</a></li>')
     })
 }
