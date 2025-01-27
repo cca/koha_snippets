cb6a8622c19cef537fd63b91f1463c707a788f56
phette23 <phette23@gmail.com>
2025-01-27 14:37:54 -0800

    Revert "fix: hide 'new from z39' while it's broken"
    Bug fixed upstream
    This reverts commit 741f3f21f27c46a0b70ad9987947520d3759ff62.

--- admin-js/catalog-home.js
+++
@@ -1,5 +0,0 @@
-// hide "new from z39.50/SRU" while it's broken
-// https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=37947
-if (location.pathname.match('/cgi-bin/koha/cataloguing/cataloging-home.pl')) {
-    $('#z3950search').parent().hide()
-}
