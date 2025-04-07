07b6158f346c8ab9b0e1ee3a9fb37f3adc489ac4
phette23 <phette23@gmail.com>
2025-04-07 15:57:19 -0700

    fix: non-SSO logins, closes #47
    keep the local login form on the auth page instead of wiping & recreating it
    several changes to authentication text but might need further edits

--- catalog-js/login-cas.js
+++ catalog-js/login-cas.js
@@ -1,34 +1,25 @@
 // interactions for the login page:
-// - detect failed CAS login
-// - button to show more information
-// runs on document load
+// - improve language for SSO login
+// - catch SSO errors & make more informative
+// see also: html/OpacLoginInstructions.html
 $(() => {
-    let login = $('#cca-login')
+    let ssoLoginButton = '<div id="sso-login-js"><p><a class="btn btn-primary btn-lg" href="https://sso.cca.edu/cas/login?service=https%3A%2F%2Flibrary.cca.edu%2Fcgi-bin%2Fkoha%2Fopac-user.pl">Login with your CCA Account</a></p><p>Most users should use the button above. If you are an alumnus or pre-college student, you might have a local account which is used below.</p></div>'
     // only run if the "NoLoginInstructions" HTML is present on the page
-    if (login.length) {
-        let uncomment = (id) => {
-            $(id).html($(id).html().replace('<!--', '').replace('-->', ''))
-        }
+    if ($('#OpacLoginInstructions').length) {
+        // remove existing SSO login button & add a new one up top
+        $('.sso-login').remove()
+        $('#opac-auth').prepend(ssoLoginButton)
 
-        // wipe out everything _except alerts_, swap in our custom HTML
-        login.remove()
-        // for some reason this warning no longer is an alert...
-        let failedCAS = $('p:contains("Sorry, the CAS login failed")')
-        failedCAS.addClass('alert').html(`Sorry, your CCA login failed. It's likely
-        that you don't have a library account yet or have one under a different
-        username. Please <a href="https://libraries.cca.edu/about-us/about-us/ask-a-librarian/">contact us</a>
-        for assistance.`)
-        let alerts = $('#opac-auth .alert')
-        $('#opac-auth').html('')
-            .append('<br>').append(alerts).append(login)
+        // Remove redundant or misleading text ("CAS" is meaningless to our users)
+        $('h1:contains("Log in to your account")').remove()
+        $('.cas_title').remove()
+        $('.cas_url').parent().remove()
+        $('p:contains("If you do not have a CAS account")').text('If you do not have an active CCA account, you can use your library catalog login below. If you are not sure what your login is, read the details at the bottom of the page.')
 
-        // hide everything except elements with class=show-initially
-        let noshow = login.children(':not(.show-initially)')
-        noshow.hide()
-        // when js-open btn is clicked, reveal hidden elements
-        $('.js-open').click(() => {
-            noshow.show('slow')
-            uncomment('#uncomment')
-        })
+        // CAS login failed, example:
+        // https://library.cca.edu/cgi-bin/koha/opac-user.pl?ticket=faketicket
+        $('.cas_invalid').parent().removeClass('alert-info').addClass('alert-danger')
+        $('.cas_invalid').before('<h2>Login Failed</h2>')
+        $('.cas_invalid').html(`The most common reason for this is that you are not a current CCA student or employee. If you are a CCA student or employee, please <a href="https://libraries.cca.edu/about-us/about-us/ask-a-librarian/">contact us</a>. If you are an alumnus or pre-college student, you might have a local account specific to the library catalog, which uses the form below.`)
     }
 })
--- catalog-js/passwd.js
+++ catalog-js/passwd.js
@@ -1,8 +1,5 @@
 // "change your password" page, currently only Alumni have access
 if (location.pathname.match('/cgi-bin/koha/opac-passwd.pl')) {
-    let msg = `<br>Note that this page only changes your Library Catalog password.
-        Some Alumni may sign in with their CCA password; this page does not reset that
-        password, instead see <a href="https://accounts.cca.edu/reset_password/">
-        the CCA accounts website</a>.`
+    let msg = `<br>Note that this page only changes your library catalog password. Some alumni sign in with their CCA password; this page does not reset that password, instead see <a href="https://accounts.cca.edu/reset_password/">the CCA accounts website</a>.`
     $('#mainform .alert-info').append(msg)
 }
