3b298cc8f7f05637514f71763f590fd64a67809d
phette23 <phette23@gmail.com>
2025-06-09 14:25:29 -0700

    feat: ILL required fields, ref #48
    required all title & author fields for various types
    also remove 'Other' type from dropdown menu because
    it has no fields (only custom fields? which we hide)

--- catalog-js/ill.js
+++ catalog-js/ill.js
@@ -6,7 +6,26 @@ if (location.pathname.match('/cgi-bin/koha/opac-illrequests.pl')) {
         $('#branchcode').val('SF')
         $('#branchcode').attr('disabled', true)
     }
+
+    // Remove "Other" from the Type dropdown b/c it has no fields
+    $('#type').find('option[value="other"]').remove()
+
     // Hide Custom Fields (no class on parent <fieldset>)
-    $('#add-new-fields').parent().hide()
+    $('#add-new-fields').parent().hide();
+
+    // Required fields
+    ['article_author'
+        , 'article_title'
+        , 'author'
+        , 'chapter'
+        , 'chapter_author'
+        , 'paper_author'
+        , 'paper_title'
+        , 'title']
+        .forEach(field => {
+            $(`label[for="${field}"]`).addClass('required')
+            $(`input[name="${field}"]`).attr('required', true).addClass('required')
+        })
+
     // TODO hide unneeded metadata fields for different request types
 }
