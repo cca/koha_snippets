1f3c6c35ecd0aa5ea7ee4f6d881e5193764cdd2c
phette23 <phette23@gmail.com>
2025-06-09 09:58:26 -0700

    feat: hide ILL Custom Fields
    ref #48

--- catalog-js/ill.js
+++ catalog-js/ill.js
@@ -1,9 +1,12 @@
 // https://library.cca.edu/cgi-bin/koha/opac-illrequests.pl?op=add_form&backend=Standard
-// Ensure the SF branch is the only option for ILL requests
 if (location.pathname.match('/cgi-bin/koha/opac-illrequests.pl')) {
+    // Ensure SF branch is the only option for ILL requests
     let branchInput = $('#branchcode')
     if (branchInput.length) {
         $('#branchcode').val('SF')
         $('#branchcode').attr('disabled', true)
     }
+    // Hide Custom Fields (no class on parent <fieldset>)
+    $('#add-new-fields').parent().hide()
+    // TODO hide unneeded metadata fields for different request types
 }
