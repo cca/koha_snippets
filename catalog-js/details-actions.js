719221dc46122ef7dd7cf570f59ff33bccc763a3
phette23 <phette23@gmail.com>
2025-06-09 12:43:20 -0700

    refactor: far simpler CCA login redirects
    rather than custom logic and storage items for each path we want to
    redirect, we now simply check if we are on a path we want to redirect
    and store its full URL to redirect to post-authentication
    this greatly simplifies login-redirects.js
    
    this also removes the save-to-lists.js script which is not necessary on
    search pages anymore, putting the opac-detail logic there into the
    details-actions.js file with similar logic
    
    this also redirects ILL requests after authentication, ref #48

--- catalog-js/details-actions.js
+++ catalog-js/details-actions.js
@@ -1,36 +1,51 @@
 // changes to the right-side #action menu of bib detail page
 if (location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
-    $(()=>{
-        // Customizations to the right-side "Actions" menu
-        // remove "print", "suggest for purchase", & "save record" links
-        $('.print-large').parent('li').hide()
-        $('#export').parent('li').hide()
+    const searchParams = new URLSearchParams(location.search)
+        , biblionumber = searchParams.get("biblionumber")
+        , actions = $('#action')
+        , itemTypes = $('#holdingst .itype img')
+            .map((i, el) => $(el).attr('title'))
+            .toArray()
 
-        // hide suggest for purchase unless every item is lost in some way
-        let statuses = $('#holdingst td.status')
-        if (!statuses.toArray().every(el => !!el.querySelector('.lost'))) {
-            $('.suggest_for_purchase').parent('li').hide()
+    // Customizations to the right-side "Actions" menu
+    // remove "print", "suggest for purchase", & "save record" links
+    $('.print-large').parent('li').hide()
+    $('#export').parent('li').hide()
+
+    // Only add these actions if biblionumber is numeric/safe
+    if (biblionumber && biblionumber.match(/^\d+$/)) {
+        // Add item to list when unauthenticated
+        if (!actions.find('.addtoshelf').length) {
+            actions.prepend(`<li><a class="addtoshelf btn btn-link btn-lg" href="/cgi-bin/koha/opac-addbybiblionumber.pl?biblionumber=${biblionumber}"><i class="fa fa-fw fa-list" aria-hidden="true"></i> Save to your lists</a></li>`)
         }
 
-        // remove "Request article" link for non-periodical item types
-        let itypes = $('#holdingst .itype img').map((i, el) => $(el).attr('title')).toArray()
-        let type = $('.results_summary.type').text().toLowerCase()
-        if (itypes.length && !itypes.some(itype => itype.match(/Periodical/)) || !type.match("continuing resource")) {
-            $('.article_request').parent('li').remove()
-        }
+        // Permalink
+        actions.append(`<li><a class="btn btn-link btn-lg" id="permalink" href="${location.pathname + '?biblionumber=' + biblionumber}"><i class="fa fa-fw fa-link"></i> Permanent Link</a></li>`)
 
-        // add permalink - pull biblionumber from unapi tag
-        // cast to string to prevent XSS through DOM
-        let biblionumber =  String($('.unapi-id').attr('title').split(':')[2])
-        let permalink = String(location.pathname + '?biblionumber=' + biblionumber)
-        $('#action').append(`<li><a class="btn btn-link btn-lg" id="permalink" href="${permalink}"><i class="fa fa-fw fa-link"></i> Permanent Link</a></li>`)
-
-        // if 1) Place Hold not present & 2) at least one item is not a type that doesn't allow holds
-        // show the Place Hold action _even for unauthenticated users_ (Koha does not by default).
+        // Show the Place Hold action _even for unauthenticated users_ if
+        // 1) Place Hold not present
+        // 2) At least one item is not a type that doesn't allow holds
+        // 3) At least one item lacks a "notforloan" status
         const noHoldTypes = ['Equipment', 'Ebook', 'Object']
-        let hasHoldableItem = itypes.some(itype => !noHoldTypes.includes(itype))
-        if (!$('#action .reserve').length && hasHoldableItem) {
-            $('#action').prepend(`<li><a class="btn btn-link btn-lg reserve" href="/cgi-bin/koha/opac-reserve.pl?biblionumber=${biblionumber}"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> Place Hold</a></li>`)
+            , hasHoldableItem = itemTypes.some(t => !noHoldTypes.includes(t))
+            , hasNonNFLItem = $('td.status .item-status')
+                .map((i, el) => $(el)[0].classList)
+                .toArray() // classList is a DOMTokenList with .contains(), not .includes()
+                .some(classes => !classes.contains('notforloan'))
+        if (!actions.find('.reserve').length && hasHoldableItem && hasNonNFLItem) {
+            actions.prepend(`<li><a class="btn btn-link btn-lg reserve" href="/cgi-bin/koha/opac-reserve.pl?biblionumber=${biblionumber}"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> Place Hold</a></li>`)
         }
-    })
+    }
+
+    // hide suggest for purchase unless every item is lost
+    let statuses = $('#holdingst td.status')
+    if (!statuses.toArray().every(el => !!el.querySelector('.lost'))) {
+        $('.suggest_for_purchase').parent('li').hide()
+    }
+
+    // remove "Request article" link for non-periodical item types
+    let type = $('.results_summary.type').text().toLowerCase()
+    if (itemTypes.length && !itemTypes.some(t => t.match(/Periodical/)) || !type.match("continuing resource")) {
+        $('.article_request').parent('li').remove()
+    }
 }
--- catalog-js/login-redirects.js
+++ catalog-js/login-redirects.js
@@ -1,85 +1,31 @@
 // actions that require a login don't redirect back to the action post-CAS
 // this script uses session storage to remember what's going on & redirect
 // users back to the appropriate place for these actions:
-// 1) back to opac-reserve.pl?biblionumber=N when placing hold
-// 2) back to opac-suggestions.pl?op=add when making purchase suggestion
-// 3) back to opac-request-article.pl?biblionumber(s)=N when requesting an article
-// 4) back to opac-restrictedpage.pl (which doesn't require any parameter passing)
-let onLoginScreen = !!$('#opac-login-page').length
-    , search = new URLSearchParams(location.search)
-    , bibs
-    , bib
-    // clear all sessionStorage entries like "cca_*"
-    , clearCCAStorage = () => {
-        for (let i = 0; i < sessionStorage.length; i++) {
-            let key = sessionStorage.key(i)
-            if (key.match(/^cca_/)) {
-                sessionStorage.removeItem(key)
-            }
-        }
-    }
+// Adding to a list: opac-addbybiblionumber.pl?biblionumber=N
+// ILL request: opac-illrequests.pl?op=add_form
+// Purchase suggestion: opac-suggestions.pl?op=add_form
+// Hold: opac-reserve.pl?biblionumber=N
+// Reporting a problem: opac-reportproblem.pl
+// Requesting an article: opac-request-article.pl?biblionumber(s)=N
+// Restricted page: opac-restrictedpage.pl
+const onLoginScreen = !!$('#opac-login-page').length
+    , STORAGE_KEY = 'cca_redirect'
+    , redirect_paths = [ /^\/cgi-bin\/koha\/opac-addbybiblionumber\.pl/
+        , /^\/cgi-bin\/koha\/opac-illrequests\.pl/
+        , /^\/cgi-bin\/koha\/opac-suggestions\.pl/
+        , /^\/cgi-bin\/koha\/opac-reserve\.pl/
+        , /^\/cgi-bin\/koha\/opac-reportproblem\.pl/
+        , /^\/cgi-bin\/koha\/opac-request-article\.pl/
+        , /^\/cgi-bin\/koha\/opac-restrictedpage\.pl/
+    ]
 
-// 1) record bib number when we're trying to place a hold but not logged in
-if (onLoginScreen && location.pathname.match('/cgi-bin/koha/opac-reserve.pl')) {
-    // we always clear storage first in case multiple actions build up
-    // e.g. first they place hold, sign out, request article in same session
-    clearCCAStorage()
-    // URL structure is slightly different for multiple holds, biblionumbers
-    // are listed in one parameter and forward-slash separated
-    sessionStorage.setItem('cca_bib_hold', search.get('biblionumber'))
-    sessionStorage.setItem('cca_bib_holds', search.get('biblionumbers'))
-} else if (location.pathname.match('/cgi-bin/koha/opac-user.pl') && (sessionStorage.getItem('cca_bib_hold') || sessionStorage.getItem('cca_bib_holds'))) {
-    bib = sessionStorage.getItem('cca_bib_hold')
-    bibs = sessionStorage.getItem('cca_bib_holds')
-    // clear storage then go to appropriate reserve page
-    sessionStorage.removeItem('cca_bib_hold')
-    sessionStorage.removeItem('cca_bib_holds')
-    // string "null" is stored if absent b/c storage can only hold strings
-    if (bib && bib !== 'null') {
-        location = '/cgi-bin/koha/opac-reserve.pl?biblionumber=' + bib
-    } else if (bibs && bibs !== 'null') {
-        location = '/cgi-bin/koha/opac-reserve.pl?biblionumbers=' + bibs
-    }
-}
-
-// 2) we're trying to make a purchase suggestion but we're not logged in
-// note we need to use location.href because we want to match query string too
-if (onLoginScreen && location.href.match(/\/cgi-bin\/koha\/opac-suggestions.pl\?op=add/)) {
-    clearCCAStorage()
-    sessionStorage.setItem('cca_suggestion', true)
-} else if (location.pathname.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.getItem('cca_suggestion')) {
-    // clear storage, go to purchase suggestions form
-    sessionStorage.removeItem('cca_suggestion')
-    location = '/cgi-bin/koha/opac-suggestions.pl?op=add'
-}
-
-// 3) we're trying to make an article request but we're not logged in
-if (onLoginScreen && location.pathname.match('/cgi-bin/koha/opac-request-article.pl')) {
-    clearCCAStorage()
-    sessionStorage.setItem('cca_article_request', search.get('biblionumber'))
-} else if (location.pathname.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.getItem('cca_article_request')) {
-    bib = sessionStorage.getItem('cca_article_request')
-    // clear storage, go to appropriate article requests page
-    sessionStorage.removeItem('cca_article_request')
-    location = '/cgi-bin/koha/opac-request-article.pl?biblionumber=' + bib
-}
-
-// 4) we're trying to access the resticted page
-if (onLoginScreen && location.pathname.match('/cgi-bin/koha/opac-restrictedpage.pl')) {
-    clearCCAStorage()
-    sessionStorage.setItem('cca_restricted_page', true)
-} else if (location.pathname.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.getItem('cca_restricted_page')) {
-    // clear storage, return to restricted page
-    clearCCAStorage()
-    location = '/cgi-bin/koha/opac-restrictedpage.pl'
-}
-
-// 4) we're trying to report a problem
-if (onLoginScreen && location.pathname.match('/cgi-bin/koha/opac-reportproblem.pl')) {
-    clearCCAStorage()
-    sessionStorage.setItem('cca_report_problem', true)
-} else if (location.pathname.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.getItem('cca_report_problem')) {
-    // clear storage, return to restricted page
-    clearCCAStorage()
-    location = '/cgi-bin/koha/opac-reportproblem.pl'
+// We are on the login page but the path matches one of our redirects
+if (onLoginScreen && redirect_paths.some(p => location.pathname.match(p))) {
+    // Save the full URL of the page we're on
+    sessionStorage.setItem(STORAGE_KEY, location.href)
+} else if (location.pathname.match('/cgi-bin/koha/opac-user.pl') && sessionStorage.getItem(STORAGE_KEY)) {
+    // Clear storage and go to the stored URL
+    const href = sessionStorage.getItem(STORAGE_KEY)
+    sessionStorage.removeItem(STORAGE_KEY)
+    location = href
 }
