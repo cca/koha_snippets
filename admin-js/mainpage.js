d48623d98b76364bca34009a508d3987ba131119
phette23 <phette23@gmail.com>
2025-07-21 16:28:40 -0700

    feat: new ILL requests link in pending area

--- admin-js/mainpage.js
+++ admin-js/mainpage.js
@@ -7,15 +7,26 @@ if (location.pathname.match('/cgi-bin/koha/mainpage.pl') || location.pathname ==
             .append('<li><a class="icon_general icon_serials" href="/cgi-bin/koha/circ/article-requests.pl?branchcode="><i class="fa fa-fw fa-newspaper"></i>Article requests</a></li>')
             .append('<li><a class="icon_general icon_acquisitions" href="/cgi-bin/koha/suggestion/suggestion.pl?branchcode=__ANY__#ACCEPTED"><i class="fa fa-fw fa-gift"></i>Purchase Suggestions</a></li>')
 
-        // if there is an ILL request with status = NEW, highlight the ILL module biglink
+        // if there are ILL requests with status = NEW, highlight the ILL module biglink
         // only run for staff who have ILL access (can see the biglink)
         const illBiglink = $('.biglinks-list .icon_ill')
         if (illBiglink.length) {
             fetch("/api/v1/ill/requests?_order_by=-requested_date")
                 .then(response => response.json())
                 .then(requests => {
-                    if (requests.some(request => request.status === "NEW")) {
+                    const numNewRequests = requests.filter(r => r.status === "NEW").length
+                    if (numNewRequests) {
                         illBiglink.css("font-weight", "bold").append('<span style="color: maroon;"> *</span>')
+
+                        // add a link beside suggestions/article requests pending
+                        const areaPending = $('#area-pending')
+                        const newILLHTML = `<div class="pending-info" id="illrequests_pending">New ILL requests: <a href="/cgi-bin/koha/ill/ill-requests.pl"> <span class="pending-number-link">${numNewRequests}</span></a></div>`
+                        if (areaPending.length) {
+                            areaPending.append(newILLHTML)
+                        } else {
+                            // no good selector hooks
+                            $('.row .col-sm-12').eq(0).append(`<div id="area-pending" class="page-section">${newILLHTML}</div>`)
+                        }
                     }
                 })
                 .catch(e => {
