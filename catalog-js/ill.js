579ce0aec00308eeaf791541ce04ebc09037c29e
phette23 <phette23@gmail.com>
2025-08-29 09:54:14 -0700

    fix: ILL catalog JS improvements
    only run add form modifications when appropriate
    also change the DOI label on the view page

--- catalog-js/ill.js
+++ catalog-js/ill.js
@@ -1,32 +1,37 @@
 // https://library.cca.edu/cgi-bin/koha/opac-illrequests.pl?op=add_form&backend=Standard
 if (location.pathname.match('/cgi-bin/koha/opac-illrequests.pl')) {
-    // Ensure SF branch is the only option for ILL requests
-    let branchInput = $('#branchcode')
-    if (branchInput.length) {
-        $('#branchcode').val('SF')
-        $('#branchcode').attr('disabled', true)
+    const params = new URLSearchParams(location.search)
+
+    if (params.get('op') === 'add_form') {
+        // Ensure SF branch is the only option for ILL requests
+        let branchInput = $('#branchcode')
+        if (branchInput.length) {
+            $('#branchcode').val('SF')
+            $('#branchcode').attr('disabled', true)
+        }
+
+        // We do not fulfill "Journal", "Conference", or "Other" (has no fields)
+        // type requests so we remove them all from the dropdown menu
+        $('#type').find('option[value="conference"], option[value="journal"], option[value="other"]').remove()
+
+        // Hide Custom Fields (no class on parent <fieldset>)
+        $('#add-new-fields').parent().hide();
+
+        // Required fields
+        ['article_author'
+            , 'article_title'
+            , 'author'
+            , 'chapter'
+            , 'paper_author'
+            , 'paper_title'
+            , 'title']
+            .forEach(field => {
+                $(`label[for="${field}"]`).addClass('required')
+                $(`input[name="${field}"]`).attr('required', true).addClass('required')
+            })
     }
 
-    // We do not fulfill "Journal", "Conference", or "Other" (has no fields)
-    // type requests so we remove them all from the dropdown menu
-    $('#type').find('option[value="conference"], option[value="journal"], option[value="other"]').remove()
-
-    // Hide Custom Fields (no class on parent <fieldset>)
-    $('#add-new-fields').parent().hide();
-
-    // Required fields
-    ['article_author'
-        , 'article_title'
-        , 'author'
-        , 'chapter'
-        , 'paper_author'
-        , 'paper_title'
-        , 'title']
-        .forEach(field => {
-            $(`label[for="${field}"]`).addClass('required')
-            $(`input[name="${field}"]`).attr('required', true).addClass('required')
-        })
-
     // change DOI label to "DOI / Link
-    $('label:contains(DOI:)').text("DOI / Link:")
+    $('label:contains(DOI:)').text("DOI / Link:") // add form
+    $('.requestattr-DOI .label').text("DOI / Link:") // view details
 }
