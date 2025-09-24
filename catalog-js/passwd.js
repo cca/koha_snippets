1a84821e2d720a2cdd38b129c13f46986f97edfb
phette23 <phette23@gmail.com>
2025-09-24 10:35:08 -0700

    fix: note on password recovery page

--- catalog-js/passwd.js
+++ catalog-js/passwd.js
@@ -1,5 +1,9 @@
 // "change your password" page, currently only Alumni have access
+let passwdMsg = `<br>This page only changes your library catalog password. Some alumni sign in with their CCA password; to reset that password instead, see <a href="https://accounts.cca.edu/reset_password/">Accounts</a>.`
 if (location.pathname.match('/cgi-bin/koha/opac-passwd.pl')) {
-    let msg = `<br>Note that this page only changes your library catalog password. Some alumni sign in with their CCA password; this page does not reset that password, instead see <a href="https://accounts.cca.edu/reset_password/">the CCA accounts website</a>.`
-    $('#mainform .alert-info').append(msg)
+    $('#mainform .alert-info').eq(0).append(passwdMsg)
+}
+// password recovery page, now linked off of login page
+if (location.pathname.match('/cgi-bin/koha/opac-password-recovery.pl')) {
+    $('#password-recovery p').eq(0).append(passwdMsg)
 }
