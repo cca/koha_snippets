9132ce09ecc94770afbbc91a4ec2ff5290afb0ef
phette23 <phette23@gmail.com>
2025-09-24 17:20:00 -0700

    fix: limit to 5 online journal links
    closes #50
    also fixes some bugs in the serials-solutions-holdings.js code
    related to inconsistent data in the 360 API: now this should
    work even if a linkGroup's holdingData lacks normalized dates,
    or a start date, or if item.url is not an array

--- catalog-js/serials-solutions-holdings.js
+++ catalog-js/serials-solutions-holdings.js
@@ -14,49 +14,63 @@ if (location.pathname.match('/cgi-bin/koha/opac-detail.pl')) {
         // base of 360 Link XML API proxy
         let ss_url = 'https://libraries.cca.edu/sersol/?issn=' + issn
 
-        $.get(ss_url, function (data, status, xhr) {
+        fetch(ss_url)
+            .then(response => response.json())
+            .then(data => {
             // first off—did we get any results from Serials Solutions?
             if (data.results) {
                 // we'll build a list of formatted holdings in HTML
                 let htmlist = []
 
                 // function takes an entry under linkGroup & adds it to htmllist
-                let formatEntry = (item) => {
-                    let source = item.holdingData
+                const formatEntry = (item) => {
+                    const source = item.holdingData
                     // skip our library catalog holdings
                     if (!source.databaseName.match('CCA Print Holdings')) {
-                        let start = source.normalizedData.startDate.substr(0, 4)
-                        let end = (source.normalizedData.endDate && source.normalizedData.endDate.substr(0, 4)) || 'present'
+                        const nd = source.normalizedData
+                        let coverage = ''
+                        if (!nd) {
+                            coverage = ''
+                        } else if (nd) {
+                            // Either date can be undefined
+                            // Ex. ARTMargins has no startDate https://libraries.cca.edu/sersol/?issn=2162-2574
+                            // We are OK with "to 2024 in X" if no startDate
+                            coverage = (nd.startDate && nd.startDate.substr(0, 4)) || ''
+                            coverage += ' to '
+                            coverage += (nd.endDate && nd.endDate.substr(0, 4)) || 'present'
+                        }
 
-                        // we want the _last_ entry in the item.url array
-                        htmlist.push(`${start} to ${end} in <a property="url" href="${item.url.pop()['#text']}">${source.databaseName}</a>`)
+                        // item.url can be object or array, we want last entry if it's an array
+                        htmlist.push(`${coverage} in <a property="url" href="${item.url['#text'] || item.url.pop()['#text']}">${source.databaseName}</a>`)
                     }
                 }
-                /* jshint ignore:start */
-                let formatLinkGroup = (result) => {
+                const formatLinkGroup = (result) => {
                     if (result.linkGroups && result.linkGroups.linkGroup) {
                         // linkGroup can be an object or array
-                        let lg = result.linkGroups.linkGroup
-
-                        // shut up jshint, this is elegant
+                        // Ex. Object (ARTMargins): https://libraries.cca.edu/sersol/?issn=2162-2574
+                        // Ex. Array (Nat Geo): https://libraries.cca.edu/sersol/?issn=0027-9358
+                        const lg = result.linkGroups.linkGroup
                         Array.isArray(lg) ? lg.forEach(formatEntry) : formatEntry(lg);
                     }
                 }
 
-                // data.results.result can be an object or array
-                let result = data.results.result
+                // data.results.result can be an object or array (same examples above work)
+                const result = data.results.result
                 Array.isArray(result) ? result.forEach(formatLinkGroup) : formatLinkGroup(result);
-                /* jshint ignore:end */
 
-                // if .online_resources exists, add to it, otherwise create it
-                let orspan = $('.results_summary.online_resources')
-                if (orspan.length > 0 && htmlist.length > 0) {
-                    orspan.append('| ' + htmlist.join(' | '))
-                } else if (htmlist.length > 0) {
-                    $('.record').append(`<span class="results_summary online_resources"><span class="label">Online resources: </span>${htmlist.join(' | ')}</span>`)
+                // truncate htmllist to 5 entries, example (Nat Geo):
+                // https://library.cca.edu/cgi-bin/koha/opac-detail.pl?biblionumber=22116
+                if (htmlist.length > 5) {
+                    htmlist = htmlist.slice(0, 5)
+                    htmlist.push(`See <a href="https://ey7mr5fu9x.search.serialssolutions.com/ejp/?libHash=EY7MR5FU9X#/search/?searchControl=title&searchType=issn_equals&criteria=${issn}">Journal Finder</a> for more...`)
                 }
 
-            } // end check for data.results
-        }) // end $.get
-    } // end check for ISSN
+                // if .online_resources exists, add to it, otherwise create it
+                const orspan = $('.results_summary.online_resources')
+                if (htmlist.length) {
+                    orspan.length ? orspan.append('| ' + htmlist.join(' | ')) : $('.record').append(`<span class="results_summary online_resources"><span class="label">Online resources: </span>${htmlist.join(' | ')}</span>`)
+                }
+            }
+        }).catch(e => console.error("Error fetching 360 data: ", e))
+    }
 }
