6f311b968d9cb7a4c9ca33c5a0423e2b19b24ea9
phette23 <phette23@gmail.com>
2026-09-02 09:35:16 -0700

    feat: re-enable ILL
    reduce ILL limit check to last 3 months
    this seems more fair given break months etc.

--- catalog-js/ill-limit.js
+++ catalog-js/ill-limit.js
@@ -14,17 +14,17 @@ if (location.pathname.match('/cgi-bin/koha/opac-illrequests.pl')) {
     if (patronId) {
         // other parameters: _page, _match=contains, could also order by illrequest_id descending
         // https://library-staff.cca.edu/api/v1/.html#op-get--public-patrons--patron_id--ill-requests
-        // we only need to check the first three and see if they're < a year old
+        // we only need to check the first three and see if they're < a semester (three months) old
         fetch(
             `/api/v1/public/patrons/${patronId}/ill/requests?_per_page=${limit}&_order_by=-me.requested_date`,
         )
             .then(resp => resp.json())
             .then(requests => {
-                let fourMonthsAgo = new Date()
-                fourMonthsAgo = new Date(fourMonthsAgo.setMonth(
-                  fourMonthsAgo.getMonth() - 4,
+                let threeMonthsAgo = new Date()
+                threeMonthsAgo = new Date(threeMonthsAgo.setMonth(
+                  threeMonthsAgo.getMonth() - 3,
                 ))
-                let recentRequests = requests.filter(req => new Date(req.requested_date) > fourMonthsAgo)
+                let recentRequests = requests.filter(req => new Date(req.requested_date) > threeMonthsAgo)
                 console.debug(`Patron ${patronId} has ${recentRequests.length} recent requests of last ${requests.length} ILL requests.`)
                 if (recentRequests.length >= limit) overLimit()
             })
